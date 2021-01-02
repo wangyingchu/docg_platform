704c858d5b40dee57cde6efea2d6df3f0fede252
wangyingchu <yingchuwang@gmail.com>
2021-01-03 00:15:34 +0800

    update for realm demo generator

--- docg_realmExample/src/main/java/com/viewfunction/docg/realmExample/generator/UndergroundPipelineNetwork_Realm_Generator.java
+++ docg_realmExample/src/main/java/com/viewfunction/docg/realmExample/generator/UndergroundPipelineNetwork_Realm_Generator.java
@@ -2,6 +2,10 @@ package com.viewfunction.docg.realmExample.generator;
 
 public class UndergroundPipelineNetwork_Realm_Generator {
 
+    private static final String PipelineConceptionType = "Pipeline";
+    private static final String PipePointConceptionType = "PipePoint";
+    private static final String PipeTubulationConceptionType = "PipeTubulation";
+
     public static void main(String[] args) {
 
     }
