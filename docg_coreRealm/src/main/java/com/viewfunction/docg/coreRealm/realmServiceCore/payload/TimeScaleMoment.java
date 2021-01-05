bbae7f383c9a53b1304c7e6d21aaaa181e3751c0
wangyingchu <yingchuwang@gmail.com>
2021-01-06 00:05:50 +0800

    update for TimeScaleFeatureSupportable

---
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/payload/TimeScaleMoment.java
@@ -0,0 +1,96 @@
+package com.viewfunction.docg.coreRealm.realmServiceCore.payload;
+
+public class TimeScaleMoment {
+
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+    private int second;
+
+    public TimeScaleMoment(){}
+
+    public TimeScaleMoment(int year,int month){
+        this.year = year;
+        this.month = month;
+    }
+
+    public TimeScaleMoment(int year,int month,int day){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public TimeScaleMoment(int year, int month,int day,int hour){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+    }
+
+    public TimeScaleMoment(int year, int month,int day,int hour,int minute){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public TimeScaleMoment(int year, int month,int day,int hour,int minute,int second){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+}
--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/TimeFlow.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/TimeFlow.java
@@ -1,5 +1,9 @@
 package com.viewfunction.docg.coreRealm.realmServiceCore.term;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.TimeScaleMoment;
+
+import java.util.LinkedList;
+
 public interface TimeFlow {
 
     public enum TimeScaleGrade {YEAR,MONTH,DAY,HOUR,MINUTE,SECOND,WEEK}
@@ -7,12 +11,26 @@ public interface TimeFlow {
     public String getTimeFlowName();
 
     public TimeScaleEntity getYearEntity(int year);
-    public TimeScaleEntity getYearEntities(int fromYear,int toYear);
+    public LinkedList<TimeScaleEntity> getYearEntities(int fromYear, int toYear);
     public TimeScaleEntity[] getYearEntities(int... year);
 
     public TimeScaleEntity getMonthEntity(int year,int month);
+    public LinkedList<TimeScaleEntity> getMonthEntities(TimeScaleMoment fromMonthMoment, TimeScaleMoment toMonthMoment);
+    public TimeScaleEntity[] getMonthEntities(TimeScaleMoment... monthMoments);
+
     public TimeScaleEntity getDayEntity(int year,int month,int day);
+    public LinkedList<TimeScaleEntity> getDayEntities(TimeScaleMoment fromDayMoment, TimeScaleMoment toDayMoment);
+    public TimeScaleEntity[] getDayEntities(TimeScaleMoment... dayMoments);
+
     public TimeScaleEntity getHourEntity(int year, int month,int day,int hour);
+    public LinkedList<TimeScaleEntity> getHourEntities(TimeScaleMoment fromHourMoment, TimeScaleMoment toHourMoment);
+    public TimeScaleEntity[] getHourEntities(TimeScaleMoment... hourMoments);
+
     public TimeScaleEntity getMinuteEntity(int year, int month,int day,int hour,int minute);
+    public LinkedList<TimeScaleEntity> getMinuteEntities(TimeScaleMoment fromMinuteMoment, TimeScaleMoment toMinuteMoment);
+    public TimeScaleEntity[] getMinuteEntities(TimeScaleMoment... minuteMoments);
+
     public TimeScaleEntity getSecondEntity(int year, int month,int day,int hour,int minute,int second);
+    public LinkedList<TimeScaleEntity> getSecondEntities(TimeScaleMoment fromSecondMoment, TimeScaleMoment toSecondMoment);
+    public TimeScaleEntity[] getSecondEntities(TimeScaleMoment... secondMoments);
 }
