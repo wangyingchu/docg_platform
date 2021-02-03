7eab9c6ed88c54f3e3e9993fe9d027d7675fd482
wangyingchu <yingchuwang@gmail.com>
2021-02-03 18:02:37 +0800

    update for docg_dataCube model

--- docg_dataCube/src/main/java/com/viewfunction/docg/dataCube/consoleApplication/ApplicationCapacityRegistry.java
+++ docg_dataCube/src/main/java/com/viewfunction/docg/dataCube/consoleApplication/ApplicationCapacityRegistry.java
@@ -1,21 +1,12 @@
 package com.viewfunction.docg.dataCube.consoleApplication;
 
-
+import com.viewfunction.docg.dataCube.applicationCapacity.DataCubeApplication;
 import com.viewfunction.docg.dataCube.consoleApplication.feature.BaseApplication;
 
 public class ApplicationCapacityRegistry {
     public static BaseApplication createConsoleApplication(String applicationFunctionName){
-        if(applicationFunctionName.equals("relationExtraction")){
-            //return new RelationExtractionApplication();
-        }
-        if(applicationFunctionName.equals("entityExtraction")){
-            //return new EntityExtractionApplication();
-        }
-        if(applicationFunctionName.equals("entityFusion")){
-            //return new EntityFusionApplication();
-        }
-        if(applicationFunctionName.equals("entityDisambiguation")){
-            //return new EntityDisambiguationApplication();
+        if(applicationFunctionName.equals("dataCubeApplication")){
+            return new DataCubeApplication();
         }
         return null;
     }
