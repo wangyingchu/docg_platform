9057e442ebcca4dd619a91b425336008bed426f3
wangyingchu <yingchuwang@gmail.com>
2021-02-04 10:05:52 +0800

    update for docg_dataCompute model

--- docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/applicationCapacity/DataCubeApplication.java
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/applicationCapacity/DataCubeApplication.java
@@ -1,7 +1,9 @@
 package com.viewfunction.docg.dataCompute.applicationCapacity;
 
 import com.viewfunction.docg.dataCompute.consoleApplication.feature.BaseApplication;
+import com.viewfunction.docg.dataCompute.consoleApplication.util.ApplicationLauncherUtil;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
 
 public class DataCubeApplication implements BaseApplication {
 
@@ -9,11 +11,18 @@ public class DataCubeApplication implements BaseApplication {
 
     @Override
     public boolean initApplication() {
+        String isClientNodeCfg= ApplicationLauncherUtil.getApplicationInfoPropertyValue("isClientNode");
+        boolean isClientNode=Boolean.parseBoolean(isClientNodeCfg);
+        if(isClientNode){
+            Ignition.setClientMode(true);
+        }
+        nodeIgnite= Ignition.start();
         return true;
     }
 
     @Override
     public boolean shutdownApplication() {
+        nodeIgnite.close();
         return true;
     }
 
