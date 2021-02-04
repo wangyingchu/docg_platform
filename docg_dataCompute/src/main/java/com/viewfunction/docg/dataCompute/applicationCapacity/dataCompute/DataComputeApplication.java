bf8c87d3393e5aec0839b8f8ea61b726139c4b13
wangyingchu <yingchuwang@gmail.com>
2021-02-04 14:18:11 +0800

    update for docg_dataCompute model

---
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/applicationCapacity/dataCompute/DataComputeApplication.java
@@ -0,0 +1,58 @@
+package com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute;
+
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
+import com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.commandProcessor.CommandProcessorFactory;
+import com.viewfunction.docg.dataCompute.consoleApplication.feature.BaseApplication;
+import com.viewfunction.docg.dataCompute.consoleApplication.feature.BaseCommandProcessor;
+import com.viewfunction.docg.dataCompute.consoleApplication.util.ApplicationLauncherUtil;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+public class DataComputeApplication implements BaseApplication {
+
+    private Ignite nodeIgnite=null;
+
+    private CoreRealm coreRealm = null;
+    private ExecutorService executor = null;
+    private Map<Object,Object> commandContextDataMap;
+
+    @Override
+    public boolean initApplication() {
+        String isClientNodeCfg= ApplicationLauncherUtil.getApplicationInfoPropertyValue("isClientNode");
+        boolean isClientNode=Boolean.parseBoolean(isClientNodeCfg);
+        if(isClientNode){
+            Ignition.setClientMode(true);
+        }
+        nodeIgnite= Ignition.start();
+        return true;
+    }
+
+    @Override
+    public boolean shutdownApplication() {
+        nodeIgnite.close();
+        return true;
+    }
+
+    @Override
+    public void executeConsoleCommand(String consoleCommand) {
+        if(consoleCommand != null){
+            String[] commandOptions = consoleCommand.split(" ");
+            if(commandOptions.length>0){
+                String command = commandOptions[0];
+                if(command.startsWith("-")||command.startsWith("--")){
+                    System.out.println("Please input valid command and options");
+                }else{
+                    String[] options = Arrays.copyOfRange(commandOptions,1,commandOptions.length);
+                    BaseCommandProcessor commandProcessor = CommandProcessorFactory.getCommandProcessor(command,this.coreRealm,this.executor,this.commandContextDataMap);
+                    if(commandProcessor!=null){
+                        commandProcessor.processCommand(command,options);
+                    }
+                }
+            }
+        }
+    }
+}
--- docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/consoleApplication/ApplicationCapacityRegistry.java
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/consoleApplication/ApplicationCapacityRegistry.java
@@ -1,12 +1,12 @@
 package com.viewfunction.docg.dataCompute.consoleApplication;
 
-import com.viewfunction.docg.dataCompute.applicationCapacity.DataCubeApplication;
+import com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.DataComputeApplication;
 import com.viewfunction.docg.dataCompute.consoleApplication.feature.BaseApplication;
 
 public class ApplicationCapacityRegistry {
     public static BaseApplication createConsoleApplication(String applicationFunctionName){
-        if(applicationFunctionName.equals("dataCubeApplication")){
-            return new DataCubeApplication();
+        if(applicationFunctionName.equals("dataComputeApplication")){
+            return new DataComputeApplication();
         }
         return null;
     }
