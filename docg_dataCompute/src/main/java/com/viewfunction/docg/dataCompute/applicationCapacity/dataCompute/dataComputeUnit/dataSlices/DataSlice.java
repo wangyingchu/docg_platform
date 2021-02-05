22886fb509329b7d31920cdcd9e3556f486734a4
wangyingchu <yingchuwang@gmail.com>
2021-02-05 17:56:37 +0800

    update for docg_dataCompute model

---
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/applicationCapacity/dataCompute/dataComputeUnit/dataSlices/DataSlice.java
@@ -0,0 +1,83 @@
+package com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.dataComputeUnit.dataSlices;
+
+import com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.dataComputeUnit.util.DataComputeConfigurationHandler;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
+import org.apache.ignite.configuration.CacheConfiguration;
+
+import java.util.List;
+
+public class DataSlice {
+
+    private static final String DUMMY_CACHE_NAME = "dummy_cache";
+
+    public static void main(String[] args){
+        try (Ignite ignite = Ignition.start(DataComputeConfigurationHandler.getDataComputeIgniteConfigurationFilePath())) {
+            print("Cache query DDL example started.");
+
+            // Create dummy cache to act as an entry point for SQL queries (new SQL API which do not require this
+            // will appear in future versions, JDBC and ODBC drivers do not require it already).
+            CacheConfiguration<?, ?> cacheCfg = new CacheConfiguration<>(DUMMY_CACHE_NAME).setSqlSchema("PUBLIC");
+
+            try (
+                    IgniteCache<?, ?> cache = ignite.getOrCreateCache(cacheCfg)
+            ) {
+                // Create reference City table based on REPLICATED template.
+                cache.query(new SqlFieldsQuery(
+                        "CREATE TABLE city (id LONG PRIMARY KEY, name VARCHAR) WITH \"template=replicated\"")).getAll();
+
+                // Create table based on PARTITIONED template with one backup.
+                cache.query(new SqlFieldsQuery(
+                        "CREATE TABLE person (id LONG, name VARCHAR, city_id LONG, PRIMARY KEY (id, city_id)) " +
+                                "WITH \"backups=1, affinity_key=city_id\"")).getAll();
+
+                // Create an index.
+                cache.query(new SqlFieldsQuery("CREATE INDEX on Person (city_id)")).getAll();
+
+                print("Created database objects.");
+
+                SqlFieldsQuery qry = new SqlFieldsQuery("INSERT INTO city (id, name) VALUES (?, ?)");
+
+                cache.query(qry.setArgs(1L, "Forest Hill")).getAll();
+                cache.query(qry.setArgs(2L, "Denver")).getAll();
+                cache.query(qry.setArgs(3L, "St. Petersburg")).getAll();
+
+                qry = new SqlFieldsQuery("INSERT INTO person (id, name, city_id) values (?, ?, ?)");
+
+                cache.query(qry.setArgs(1L, "John Doe", 3L)).getAll();
+                cache.query(qry.setArgs(2L, "Jane Roe", 2L)).getAll();
+                cache.query(qry.setArgs(3L, "Mary Major", 1L)).getAll();
+                cache.query(qry.setArgs(4L, "Richard Miles", 2L)).getAll();
+
+                print("Populated data.");
+
+                List<List<?>> res = cache.query(new SqlFieldsQuery(
+                        "SELECT p.name, c.name FROM Person p INNER JOIN City c on c.id = p.city_id")).getAll();
+
+                print("Query results:");
+
+                for (Object next : res)
+                    System.out.println(">>>    " + next);
+
+                //cache.query(new SqlFieldsQuery("drop table Person")).getAll();
+                //cache.query(new SqlFieldsQuery("drop table City")).getAll();
+
+                print("Dropped database objects.");
+            }
+            finally {
+                // Distributed cache can be removed from cluster only by #destroyCache() call.
+                //ignite.destroyCache(DUMMY_CACHE_NAME);
+            }
+
+            print("Cache query DDL example finished.");
+        }
+    }
+
+    private static void print(String msg) {
+        System.out.println();
+        System.out.println(">>> " + msg);
+    }
+
+}
---
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/applicationCapacity/dataCompute/dataComputeUnit/dataSlices/DataSlicesFactory.java
@@ -0,0 +1,145 @@
+package com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.dataComputeUnit.dataSlices;
+
+import com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.dataComputeUnit.util.DataComputeConfigurationHandler;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteCheckedException;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.QueryEntity;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
+import org.apache.ignite.configuration.CacheConfiguration;
+import org.apache.ignite.configuration.DataRegionConfiguration;
+import org.apache.ignite.configuration.DataStorageConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.logger.log4j2.Log4J2Logger;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataSlicesFactory {
+
+    public static void main(String[] args) throws IgniteCheckedException {
+
+/*
+        String isClientNodeCfg= DataComputeConfigurationHandler.getConfigPropertyValue("isClientUnit");
+        boolean isClientNode=Boolean.parseBoolean(isClientNodeCfg);
+        if(isClientNode){
+            Ignition.setClientMode(true);
+        }
+        Ignite nodeIgnite = Ignition.start(DataComputeConfigurationHandler.getDataComputeIgniteConfigurationFilePath());
+*/
+
+
+
+        final Map<String, Object> filedMap = new HashMap<>();
+        filedMap.put("field0", String.class.getName());
+        filedMap.put("field1", Integer.class.getName());
+        filedMap.put("field2", Boolean.class.getName());
+        filedMap.put("primaryKey001", String.class.getName());
+
+        IgniteConfiguration cfg = DataComputeConfigurationHandler.getDataComputeIgniteConfiguration();
+        //用户自定义值类型 设置为默认
+        QueryEntity qe = new QueryEntity(String.class.getName(), "SQL_CUSTOM_VALUE_TYPE");
+       // qe.setTableName("tableName001");
+        qe.addQueryField("primaryKey001", String.class.getName(), null);
+
+        for (Map.Entry<String, Object> filedEntry : filedMap.entrySet()) {
+            qe.addQueryField(filedEntry.getKey(), filedEntry.getValue().toString(), null);
+        }
+        qe.setKeyFieldName("primaryKey001");
+
+        CacheConfiguration cacheCfg = new CacheConfiguration()
+                .setQueryEntities(Collections.singleton(qe))
+                .setName("DEFAULT_CACHE_NAME");
+
+      //  CacheConfiguration enableCacheMetricsCfg = enableCacheMetrics(cfg, cacheCfg);
+     //   cfg.setCacheConfiguration(enableCacheMetricsCfg);
+
+        cfg.setClientMode(true);
+        Ignite ignite = Ignition.getOrStart(cfg);
+        IgniteCache cache = ignite.getOrCreateCache(cacheCfg).withKeepBinary();
+        //ignite.active();
+
+
+        Map<String, Object> input = new HashMap<>();
+        input.put("field0", "value0");
+        input.put("field1", 0);
+        input.put("field2", false);
+        input.put("primaryKey001", "pk001");
+
+
+        InsertData insertData = new InsertData("primaryKey001", input);
+        //StringBuilder sb = new StringBuilder("INSERT INTO ").append("tableName001").append(" (")
+        StringBuilder sb = new StringBuilder("INSERT INTO ").append("DEFAULT_CACHE_NAME").append(" (")
+                .append(insertData.getInsertFields()).append(") VALUES (")
+                .append(insertData.getInsertParams()).append(')');
+
+        SqlFieldsQuery qry = new SqlFieldsQuery(sb.toString());
+        qry.setArgs(insertData.getArgs());
+        //execute sql
+        cache.query(qry).getAll();
+
+
+
+        ignite.close();
+
+    }
+
+
+
+    private static class InsertData {
+        private final Object[] args;
+        private final String insertFields;
+        private final String insertParams;
+
+
+        /**
+         * @param key    Key.
+         * @param values Field values.
+         */
+        InsertData(String key, Map<String, Object> values) {
+            args = new Object[values.size() + 1];
+
+            int idx = 0;
+            args[idx++] = key;
+
+            StringBuilder sbFields = new StringBuilder("primaryKey001");
+            StringBuilder sbParams = new StringBuilder("?");
+
+            for (Map.Entry<String, Object> e : values.entrySet()) {
+                args[idx++] = e.getValue();
+                sbFields.append(',').append(e.getKey());
+                sbParams.append(", ?");
+            }
+
+            insertFields = sbFields.toString();
+            insertParams = sbParams.toString();
+        }
+
+        public Object[] getArgs() {
+            return args;
+        }
+
+        public String getInsertFields() {
+            return insertFields;
+        }
+
+        public String getInsertParams() {
+            return insertParams;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
