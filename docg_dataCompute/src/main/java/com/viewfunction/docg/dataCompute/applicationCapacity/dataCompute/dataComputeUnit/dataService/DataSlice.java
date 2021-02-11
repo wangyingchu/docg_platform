37b9ce89760f2f3a6f41feab773a78dafaf8e41d
wangyingchu <yingchuwang@gmail.com>
2021-02-12 01:21:13 +0800

    update for docg_dataCompute data service feature

--- docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/applicationCapacity/dataCompute/dataComputeUnit/dataService/DataSlice.java
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/applicationCapacity/dataCompute/dataComputeUnit/dataService/DataSlice.java
@@ -1,5 +1,6 @@
 package com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.dataComputeUnit.dataService;
 
+import com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.exception.DataSlicePropertiesStructureException;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.CacheMode;
@@ -7,9 +8,7 @@ import org.apache.ignite.cache.CachePeekMode;
 import org.apache.ignite.cache.QueryEntity;
 import org.apache.ignite.configuration.CacheConfiguration;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Set;
+import java.util.*;
 
 public class DataSlice {
 
@@ -21,10 +20,41 @@ public class DataSlice {
         this.cache = igniteCache;
     }
 
-    public void addData(){
+    public void addDataRecord(Map<String,Object> dataPropertiesValue) throws DataSlicePropertiesStructureException {
+        Map<String,String> slicePropertiesMap = getDataSlicePropertiesInfo();
+        Set<String> propertiesSet = slicePropertiesMap.keySet();
+
+        Set<String> dataPropertyNameSet = dataPropertiesValue.keySet();
+
+        for(String currentPropertyName:dataPropertyNameSet){
+            if(!propertiesSet.contains(currentPropertyName)){
+                throw new DataSlicePropertiesStructureException();
+            }
+        }
+
+        String[] arr = propertiesSet.stream().toArray(n -> new String[n]);
 
 
 
+
+
+
+
+
+
+        for(String currentPropertyName:dataPropertyNameSet){
+
+        }
+
+
+        String sliceName = this.cache.getName();
+        //SqlFieldsQuery qry = new SqlFieldsQuery("INSERT INTO city (id, name) VALUES (?, ?)");
+
+
+
+    }
+
+    public void addDataRecords(List<Map<String,Object>> dataPropertiesValueList){
         CacheConfiguration ccfg = cache.getConfiguration(CacheConfiguration.class);
         Collection<QueryEntity> entities = ccfg.getQueryEntities();
 
@@ -36,29 +66,12 @@ public class DataSlice {
             System.out.println(propertiesMap);
             Set<String> keyField = mainQueryEntity.getKeyFields();
             System.out.println(keyField);
-
         }
-
-
-
-
-
-
-
-
-
-        //this.cache.metrics().getTxKeyCollisions();
-
-
-        //this.cache.metrics().getValueType();
-        //this.cache.metrics().
-
-
     }
 
     public DataSliceMetaInfo getDataSliceMetaInfo(){
-        CacheConfiguration currentCacheConfig=this.cache.getConfiguration(CacheConfiguration.class);
-        DataSliceMetaInfo dataSliceMetaInfo =new DataSliceMetaInfo();
+        CacheConfiguration currentCacheConfig = this.cache.getConfiguration(CacheConfiguration.class);
+        DataSliceMetaInfo dataSliceMetaInfo = new DataSliceMetaInfo();
         dataSliceMetaInfo.setDataSliceName(currentCacheConfig.getName());
         dataSliceMetaInfo.setPrimaryDataCount(this.cache.size(CachePeekMode.PRIMARY));
         dataSliceMetaInfo.setBackupDataCount(this.cache.size(CachePeekMode.BACKUP));
@@ -77,5 +90,27 @@ public class DataSlice {
         dataSliceMetaInfo.setKeyClass(currentCacheConfig.getKeyType());
         dataSliceMetaInfo.setValueClass(currentCacheConfig.getValueType());
         return dataSliceMetaInfo;
+    }
+
+    private Set<String> getDataSlicePrimaryKeysInfo(){
+        CacheConfiguration ccfg = cache.getConfiguration(CacheConfiguration.class);
+        Collection<QueryEntity> entities = ccfg.getQueryEntities();
+        if(entities != null && entities.size()>0){
+            QueryEntity mainQueryEntity = entities.iterator().next();
+            Set<String> keysField = mainQueryEntity.getKeyFields();
+            return keysField;
+        }
+        return null;
+    }
+
+    private Map<String,String> getDataSlicePropertiesInfo(){
+        CacheConfiguration ccfg = cache.getConfiguration(CacheConfiguration.class);
+        Collection<QueryEntity> entities = ccfg.getQueryEntities();
+        if(entities != null && entities.size()>0){
+            QueryEntity mainQueryEntity = entities.iterator().next();
+            LinkedHashMap<String,String> propertiesMap = mainQueryEntity.getFields();
+            return propertiesMap;
+        }
+        return null;
     }
 }
