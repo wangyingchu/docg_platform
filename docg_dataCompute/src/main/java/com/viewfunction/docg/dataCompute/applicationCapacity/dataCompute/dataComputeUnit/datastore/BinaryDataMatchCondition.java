ee19d7db8f95cbe2651c91eb2759c6b32db1fb1b
wangyingchu <yingchuwang@gmail.com>
2021-02-25 17:03:01 +0800

    docg_dataCompute data slice feature clean code structure

--- docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/applicationCapacity/dataCompute/dataComputeUnit/datastore/BinaryDataMatchCondition.java
+++
@@ -1,8 +0,0 @@
-package com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.dataComputeUnit.datastore;
-
-import org.apache.ignite.binary.BinaryObject;
-
-public interface BinaryDataMatchCondition {
-
-    public boolean match(BinaryObject keyBinaryObject, BinaryObject valueBinaryObject);
-}
--- docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/applicationCapacity/dataCompute/dataComputeUnit/datastore/GeneralDataMatchCondition.java
+++
@@ -1,6 +0,0 @@
-package com.viewfunction.docg.dataCompute.applicationCapacity.dataCompute.dataComputeUnit.datastore;
-
-public interface GeneralDataMatchCondition<K,V> {
-
-    public boolean match(K keyObject, V valueObject);
-}
