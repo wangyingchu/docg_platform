4c4b0fc2ca741b78299d6662810b61bd6ad43b73
wangyingchu <yingchuwang@gmail.com>
2021-03-02 11:00:14 +0800

    update for docg_coreRealm Geospatial feature support

---
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/internal/neo4j/util/GeospatialScaleOperationUtil.java
@@ -0,0 +1,28 @@
+package com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.util;
+
+import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.GraphOperationExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GeospatialScaleOperationUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(GeospatialScaleOperationUtil.class);
+
+    public static void generateTimeFlowScaleEntities(GraphOperationExecutor workingGraphOperationExecutor, String geospatialRegionName){
+
+    }
+
+    private static void generateTimeFlowScaleEntities_Continent(){
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
