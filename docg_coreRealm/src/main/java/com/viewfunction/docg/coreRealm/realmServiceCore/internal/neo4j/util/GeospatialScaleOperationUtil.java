815868dfa364ddce11ca174b193fd5ffc7cbc3d7
wangyingchu <yingchuwang@gmail.com>
2021-03-02 17:28:54 +0800

    update for docg_coreRealm Geospatial feature support

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/internal/neo4j/util/GeospatialScaleOperationUtil.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/internal/neo4j/util/GeospatialScaleOperationUtil.java
@@ -3,14 +3,13 @@ package com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.util;
 import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
 import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.CypherBuilder;
 import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.GraphOperationExecutor;
-import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.dataTransformer.DataTransformer;
+import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.dataTransformer.GetListConceptionEntityTransformer;
 import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.dataTransformer.GetSingleConceptionEntityTransformer;
-import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.dataTransformer.GetSingleGeospatialRegionTransformer;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionEntity;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.GeospatialRegion;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.RealmConstant;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.config.PropertiesHandler;
-import org.neo4j.driver.Result;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +18,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GeospatialScaleOperationUtil {
@@ -31,11 +31,6 @@ public class GeospatialScaleOperationUtil {
     }
 
     private static void generateGeospatialScaleEntities_Continent(GraphOperationExecutor workingGraphOperationExecutor, String geospatialRegionName){
-        File file = new File(PropertiesHandler.SYSTEM_RESOURCE_ROOT+"/"+GEOSPATIAL_DATA_FOLDER+"/"+"ContinentsData.txt");
-        String[] conceptionTypeNameArray = new String[2];
-        conceptionTypeNameArray[0] = RealmConstant.GeospatialScaleEntityClass;
-        conceptionTypeNameArray[1] = RealmConstant.GeospatialScaleContinentEntityClass;
-
         GetSingleConceptionEntityTransformer getSingleConceptionEntityTransformer =
                 new GetSingleConceptionEntityTransformer(RealmConstant.GeospatialRegionClass,workingGraphOperationExecutor);
         String queryCql = CypherBuilder.matchLabelWithSinglePropertyValue(RealmConstant.GeospatialRegionClass,RealmConstant._NameProperty,geospatialRegionName,1);
@@ -50,6 +45,11 @@ public class GeospatialScaleOperationUtil {
         getSingleConceptionEntityTransformer =
                 new GetSingleConceptionEntityTransformer(RealmConstant.GeospatialScaleContinentEntityClass,workingGraphOperationExecutor);
 
+        String[] conceptionTypeNameArray = new String[2];
+        conceptionTypeNameArray[0] = RealmConstant.GeospatialScaleEntityClass;
+        conceptionTypeNameArray[1] = RealmConstant.GeospatialScaleContinentEntityClass;
+
+        File file = new File(PropertiesHandler.SYSTEM_RESOURCE_ROOT+"/"+GEOSPATIAL_DATA_FOLDER+"/"+"ContinentsData.txt");
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(new FileReader(file));
@@ -96,25 +96,98 @@ public class GeospatialScaleOperationUtil {
         }
     }
 
-    private static DataTransformer getSilentOperationDataTransformer(){
-        DataTransformer dataTransformer = new DataTransformer() {
-            @Override
-            public Object transformResult(Result result) {
-                return null;
+    private static void generateGeospatialScaleEntities_CountryRegion(GraphOperationExecutor workingGraphOperationExecutor, String geospatialRegionName){
+        Map<String,String> ContinentCode_EntityUIDMap = new HashMap<>();
+        String queryCql = CypherBuilder.matchLabelWithSinglePropertyValue(RealmConstant.GeospatialScaleContinentEntityClass,"geospatialRegion",geospatialRegionName,100000);
+
+        GetListConceptionEntityTransformer getListConceptionEntityTransformer = new GetListConceptionEntityTransformer(RealmConstant.GeospatialScaleContinentEntityClass,workingGraphOperationExecutor);
+        Object resultEntityList = workingGraphOperationExecutor.executeRead(getListConceptionEntityTransformer,queryCql);
+        if(resultEntityList != null){
+            List<ConceptionEntity> resultContinentList =  (List<ConceptionEntity>)resultEntityList;
+            for(ConceptionEntity currentConceptionEntity : resultContinentList){
+                ContinentCode_EntityUIDMap.put(
+                    currentConceptionEntity.getAttribute("code").getAttributeValue().toString(),
+                        currentConceptionEntity.getConceptionEntityUID());
             }
-        };
-        return dataTransformer;
+        }
+
+        String[] conceptionTypeNameArray = new String[2];
+        conceptionTypeNameArray[0] = RealmConstant.GeospatialScaleEntityClass;
+        conceptionTypeNameArray[1] = RealmConstant.GeospatialScaleCountryRegionEntityClass;
+
+        File file = new File(PropertiesHandler.SYSTEM_RESOURCE_ROOT+"/"+GEOSPATIAL_DATA_FOLDER+"/"+"CountriesAndRegionsData(ISO_3166_1).txt");
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String currentLine;
+            while ((currentLine = reader.readLine()) != null) {
+                if(currentLine != null){
+                    String[] countriesAndRegionInfoValueArray = currentLine.split("\\|");
+
+                    Map<String,Object> propertiesMap = new HashMap<>();
+                    String continentEntityUID = null;
+                    String _2bitCode = countriesAndRegionInfoValueArray[0].trim();
+                    String _3bitCode = countriesAndRegionInfoValueArray[1].trim();
+                    String _number = countriesAndRegionInfoValueArray[2].trim();
+                    String _ISO3122_2Code = countriesAndRegionInfoValueArray[3].trim();
+                    String _EnglishName = countriesAndRegionInfoValueArray[4].trim();
+                    String _ChineseName = countriesAndRegionInfoValueArray[5].trim();
+
+                    propertiesMap.put("twoBitCode",_2bitCode);
+                    propertiesMap.put("threeBitCode",_3bitCode);
+                    propertiesMap.put("number",_number);
+                    propertiesMap.put("ISO3122_2Code",_ISO3122_2Code);
+                    propertiesMap.put("EnglishName",_EnglishName);
+                    propertiesMap.put("ChineseName",_ChineseName);
+
+                    if(!"-".equals(countriesAndRegionInfoValueArray[6].trim())){
+                        String _belongedContinent = countriesAndRegionInfoValueArray[6].trim();
+                        propertiesMap.put("belongedContinent",_belongedContinent);
+                        continentEntityUID = ContinentCode_EntityUIDMap.get(_belongedContinent);
+                    }
+                    if(!"-".equals(countriesAndRegionInfoValueArray[7].trim())){
+                        String _capitalChineseName = countriesAndRegionInfoValueArray[7].trim();
+                        propertiesMap.put("capitalChineseName",_capitalChineseName);
+                    }
+                    if(!"-".equals(countriesAndRegionInfoValueArray[8].trim())){
+                        String _capitalEnglishName = countriesAndRegionInfoValueArray[8].trim();
+                        propertiesMap.put("capitalEnglishName",_capitalEnglishName);
+                    }
+
+                    propertiesMap.put("code",_2bitCode);
+                    propertiesMap.put("geospatialRegion",geospatialRegionName);
+                    propertiesMap.put("geospatialScaleGrade", ""+GeospatialRegion.GeospatialScaleGrade.COUNTRY_REGION);
+
+                    GetSingleConceptionEntityTransformer getSingleConceptionEntityTransformer =
+                            new GetSingleConceptionEntityTransformer(RealmConstant.GeospatialScaleCountryRegionEntityClass,workingGraphOperationExecutor);
+
+                    String createGeospatialScaleEntitiesCql = CypherBuilder.createLabeledNodeWithProperties(conceptionTypeNameArray,propertiesMap);
+                    Object newEntityRes = workingGraphOperationExecutor.executeWrite(getSingleConceptionEntityTransformer,createGeospatialScaleEntitiesCql);
+
+                    if(newEntityRes != null && continentEntityUID != null){
+                        ConceptionEntity timeScaleEventEntity = (ConceptionEntity) newEntityRes;
+                        timeScaleEventEntity.attachToRelation(continentEntityUID, RealmConstant.GeospatialScale_SpatialContainsRelationClass, null, true);
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException | CoreRealmServiceRuntimeException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
     }
 
-
-public static void main(String[] args){
-    GraphOperationExecutor graphOperationExecutor = new GraphOperationExecutor();
-    generateGeospatialScaleEntities_Continent(graphOperationExecutor,"DefaultGeospatialRegion");
-    graphOperationExecutor.close();
-}
-
-
-
-
-
+    public static void main(String[] args){
+        GraphOperationExecutor graphOperationExecutor = new GraphOperationExecutor();
+        //generateGeospatialScaleEntities_Continent(graphOperationExecutor,"DefaultGeospatialRegion");
+        generateGeospatialScaleEntities_CountryRegion(graphOperationExecutor,"DefaultGeospatialRegion");
+        graphOperationExecutor.close();
+    }
 }
