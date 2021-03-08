c4bee9a89b593da66a1e96bf929628439daca337
wangyingchu <yingchuwang@gmail.com>
2021-03-08 15:31:42 +0800

    update for documentation

--- README.md
+++ README.md
@@ -2824,7 +2824,54 @@ DOCG 数据分析平台的业务模型使用以下的术语来描述实现一个
 ---
 
 ```java
+    /**
+     * 获取当前时间刻度事件所属的时间流名称
+     *
+     * @return 时间流名称
+     */
+    public String getTimeFlowName();
 
+    /**
+     * 获取当前时间刻度事件发生的时间值
+     *
+     * @return 时间值
+     */
+    public long getReferTime();
+
+    /**
+     * 获取当前时间刻度事件的时间刻度等级
+     *
+     * @return 时间刻度等级
+     */
+    public TimeFlow.TimeScaleGrade getTimeScaleGrade();
+
+    /**
+     * 获取当前时间刻度事件的唯一ID
+     *
+     * @return 时间刻度事件唯一ID
+     */
+    public String getTimeScaleEventUID();
+
+    /**
+     * 获取当前时间刻度事件的事件备注
+     *
+     * @return 时间刻度事件事件备注
+     */
+    public String getEventComment();
+
+    /**
+     * 获取当前时间刻度事件相关的时间刻度实体
+     *
+     * @return 时间刻度实体对象
+     */
+    public TimeScaleEntity getReferTimeScaleEntity();
+
+    /**
+     * 获取当前时间刻度事件相关的常规概念实体
+     *
+     * @return 常规概念实体对象
+     */
+    public ConceptionEntity getAttachConceptionEntity();
 ```
 
 #### ➜ 领域模型数据分析
--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/TimeScaleEvent.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/TimeScaleEvent.java
@@ -4,13 +4,52 @@ import com.viewfunction.docg.coreRealm.realmServiceCore.feature.AttributesMeasur
 import com.viewfunction.docg.coreRealm.realmServiceCore.feature.ClassificationAttachable;
 
 public interface TimeScaleEvent extends AttributesMeasurable, ClassificationAttachable {
-
+    /**
+     * 获取当前时间刻度事件所属的时间流名称
+     *
+     * @return 时间流名称
+     */
     public String getTimeFlowName();
-    public long getReferTime();
-    public TimeFlow.TimeScaleGrade getTimeScaleGrade();
-    public String getTimeScaleEventUID();
-    public String getEventComment();
-    public TimeScaleEntity getReferTimeScaleEntity();
-    public ConceptionEntity getAttachConceptionEntity();
 
+    /**
+     * 获取当前时间刻度事件发生的时间值
+     *
+     * @return 时间值
+     */
+    public long getReferTime();
+
+    /**
+     * 获取当前时间刻度事件的时间刻度等级
+     *
+     * @return 时间刻度等级
+     */
+    public TimeFlow.TimeScaleGrade getTimeScaleGrade();
+
+    /**
+     * 获取当前时间刻度事件的唯一ID
+     *
+     * @return 时间刻度事件唯一ID
+     */
+    public String getTimeScaleEventUID();
+
+    /**
+     * 获取当前时间刻度事件的事件备注
+     *
+     * @return 时间刻度事件事件备注
+     */
+    public String getEventComment();
+
+    /**
+     * 获取当前时间刻度事件相关的时间刻度实体
+     *
+     * @return 时间刻度实体对象
+     */
+    public TimeScaleEntity getReferTimeScaleEntity();
+
+    /**
+     * 获取当前时间刻度事件相关的常规概念实体
+     *
+     * @return 常规概念实体对象
+     */
+    public ConceptionEntity getAttachConceptionEntity();
 }
