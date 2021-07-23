a5f8990b841903f1759bb656222821df1583a22c
wangyingchu <yingchuwang@gmail.com>
2021-07-23 16:52:06 +0800

    update for new docg analysis provider feature

--- docg_analysisProvider/src/test/scala/analysisExample/GraphAnalysisExample.scala
+++ docg_analysisProvider/src/test/scala/analysisExample/GraphAnalysisExample.scala
@@ -17,76 +17,39 @@ object GraphAnalysisExample {
     val sparkMasterLocation = AnalysisProviderApplicationUtil.getApplicationProperty("sparkMasterLocation")
     val globalDataAccessor = new GlobalDataAccessor(sparkApplicationName,sparkMasterLocation)
 
-    val connectionEdgeDF = globalDataAccessor.getDataFrameFromDataSlice("GS_SpatialConnect",CoreRealmOperationUtil.defaultSliceGroup)
-    val endPointDF = globalDataAccessor.getDataFrameFromDataSlice("MainlineEndPoint",CoreRealmOperationUtil.defaultSliceGroup)
-    val mainlineDF = globalDataAccessor.getDataFrameFromDataSlice("PermittedUseMainline",CoreRealmOperationUtil.defaultSliceGroup)
-    val middlePointDF = globalDataAccessor.getDataFrameFromDataSlice("MainlineConnectionPoint",CoreRealmOperationUtil.defaultSliceGroup)
-
-    //println(connectionEdgeDF.count())
-    //println(endPointDF.count())
-    //println(mainlineDF.count())
-    //println(middlePointDF.count())
-
     val jdbcResultSetConvertImpl = new ResultSetConvertor {
       override def convertFunction(resultSet: ResultSet): Any = {
-        (resultSet.getString(1),resultSet.getString(2))
+        (resultSet.getDouble(1),resultSet.getString(2))
       }
     }
+    val vertexRDD4 = globalDataAccessor.getVertexRDD("PermittedUseMainline",CoreRealmOperationUtil.defaultSliceGroup,jdbcResultSetConvertImpl)
+    val vRdd1 = vertexRDD4.asInstanceOf[RDD[(VertexId, (Double, String))]]
+    vRdd1.take(10).foreach(println(_))
 
-    val result = (globalDataAccessor._getJdbcRDD("GS_SpatialConnect",CoreRealmOperationUtil.defaultSliceGroup,jdbcResultSetConvertImpl)).asInstanceOf[RDD[(String,String)]]
-   // println(result.count())
-
-    /*
-    result.take(100).foreach(data=>{
-      println(data._1+" -> "+data._2)
-    })
-    */
+    val jdbcResultSetConvertImpl1 = new ResultSetConvertor {
+      override def convertFunction(resultSet: ResultSet): Any = {
+        (10000,"kokosss",new Date())
+      }
+    }
+    val edgeRDD2 = globalDataAccessor.getEdgeRDD("GS_SpatialConnect",CoreRealmOperationUtil.defaultSliceGroup,jdbcResultSetConvertImpl1)
+    val eRdd1 = edgeRDD2.asInstanceOf[RDD[Edge[(Long,Int,String,Date)]]]
+    eRdd1.take(10).foreach(println(_))
 
     val edgeRDD:RDD[Edge[(Long,String,String)]] = globalDataAccessor.getEdgeRDD("GS_SpatialConnect",CoreRealmOperationUtil.defaultSliceGroup)
-
     edgeRDD.take(10).foreach(println(_))
-
-
 
     val vertexRDD1:RDD[(VertexId, (String, String))] = globalDataAccessor.getVertexRDD("PermittedUseMainline",CoreRealmOperationUtil.defaultSliceGroup)
     val vertexRDD2:RDD[(VertexId, (String, String))] = globalDataAccessor.getVertexRDD("MainlineEndPoint",CoreRealmOperationUtil.defaultSliceGroup)
     val vertexRDD3:RDD[(VertexId, (String, String))] = globalDataAccessor.getVertexRDD("MainlineConnectionPoint",CoreRealmOperationUtil.defaultSliceGroup)
     val wholeDataVertexRDD = vertexRDD1.union(vertexRDD2).union(vertexRDD3)
     vertexRDD1.take(10).foreach(println(_))
-    //println(wholeDataVertexRDD.count())
-
 
     val networkGraph = Graph(wholeDataVertexRDD,edgeRDD)
     //networkGraph.partitionBy(PartitionStrategy.RandomVertexCut,20)
-
-
     //println(networkGraph.numEdges)
     //println(networkGraph.numVertices)
     println(networkGraph.connectedComponents(20))
 
-    val jdbcResultSetConvertImpl2 = new ResultSetConvertor {
-      override def convertFunction(resultSet: ResultSet): Any = {
-        (resultSet.getDouble(1),resultSet.getString(2))
-      }
-    }
-    val vertexRDD4 = globalDataAccessor.getVertexRDD("PermittedUseMainline",CoreRealmOperationUtil.defaultSliceGroup,jdbcResultSetConvertImpl2)
-    val xxx = vertexRDD4.asInstanceOf[RDD[(VertexId, (Double, String))]]
-    xxx.take(10).foreach(println(_))
-
-    val jdbcResultSetConvertImpl3 = new ResultSetConvertor {
-      override def convertFunction(resultSet: ResultSet): Any = {
-        (10000,"kokosss",new Date())
-      }
-    }
-
-    val edgeRDD2 = globalDataAccessor.getEdgeRDD("GS_SpatialConnect",CoreRealmOperationUtil.defaultSliceGroup,jdbcResultSetConvertImpl3)
-    //val xxx2 = edgeRDD2.asInstanceOf[Edge[Long,(Int,String,Date)]
-
-    val xxx2 = edgeRDD2.asInstanceOf[RDD[Edge[(Long,Int,String,Date)]]]
-
-    xxx2.take(10).foreach(println(_))
-
     globalDataAccessor.close()
   }
-
 }
