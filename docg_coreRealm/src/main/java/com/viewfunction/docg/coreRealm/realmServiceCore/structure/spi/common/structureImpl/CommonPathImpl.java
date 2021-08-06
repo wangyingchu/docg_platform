5848e15437ec992d5122ff56c88c4c25b023ac34
wangyingchu <yingchuwang@gmail.com>
2021-08-06 17:27:58 +0800

    update for new docg analysis provider feature

---
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/structure/spi/common/structureImpl/CommonPathImpl.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.coreRealm.realmServiceCore.structure.spi.common.structureImpl;
+
+import com.viewfunction.docg.coreRealm.realmServiceCore.structure.Path;
+
+public class CommonPathImpl implements Path {
+}
