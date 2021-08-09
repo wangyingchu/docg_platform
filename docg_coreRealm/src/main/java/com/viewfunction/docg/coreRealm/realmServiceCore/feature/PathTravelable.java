93f21ee6d094fbdb4179b2a40199b2be91c5a73f
wangyingchu <yingchuwang@gmail.com>
2021-08-09 17:54:53 +0800

    update for new docg CoreRealm features

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/PathTravelable.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/PathTravelable.java
@@ -1,4 +1,10 @@
 package com.viewfunction.docg.coreRealm.realmServiceCore.feature;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.structure.Path;
+
+import java.util.List;
+
 public interface PathTravelable {
+
+    public List<Path> expandPath(int minJump,int maxJump);
 }
--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/spi/neo4j/featureInf/Neo4JPathTravelable.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/spi/neo4j/featureInf/Neo4JPathTravelable.java
@@ -1,10 +1,27 @@
 package com.viewfunction.docg.coreRealm.realmServiceCore.feature.spi.neo4j.featureInf;
 
 import com.viewfunction.docg.coreRealm.realmServiceCore.feature.PathTravelable;
+import com.viewfunction.docg.coreRealm.realmServiceCore.structure.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public interface Neo4JPathTravelable extends PathTravelable,Neo4JKeyResourcesRetrievable {
 
     Logger logger = LoggerFactory.getLogger(Neo4JPathTravelable.class);
+
+    default public List<Path> expandPath(int minJump,int maxJump){
+
+
+        //https://neo4j.com/labs/apoc/4.1/graph-querying/expand-paths/#path-expander-paths-procedure-overview
+        String cypherProcedureString="MATCH (n) WHERE id(n)= "+this.getEntityUID()+"\n" +
+                "CALL apoc.path.expand(n, \"DOCG_GS_SpatialConnect\", null, 1, 2)\n" +
+                "YIELD path\n" +
+                "RETURN path, length(path) AS hops\n" +
+                "ORDER BY hops;";
+        return null;
+    }
+
+
 }
