fd8557d9802fb7dbea29c12561c22d21da1bb106
wangyingchu <yingchuwang@gmail.com>
2021-08-11 17:28:00 +0800

    update for new docg CoreRealm features

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/analysis/query/TravelParameters.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/analysis/query/TravelParameters.java
@@ -1,6 +1,27 @@
 package com.viewfunction.docg.coreRealm.realmServiceCore.analysis.query;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.RelationDirection;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class TravelParameters {
 
+    public enum TraversalMethod {BFS, DFS}
+
+    private List<RelationKindMatchLogic> relationKindMatchLogics;
+    private RelationDirection defaultDirectionForNoneRelationKindMatch;
+    private List<ConceptionKindMatchLogic> conceptionKindMatchLogics;
+    private int minJump;
+    private int maxJump;
     private int resultNumber;
+
+    private LinkedList<RelationKindMatchLogic> relationKindFlowLogics;
+    private LinkedList<ConceptionKindMatchLogic> conceptionKindFlowLogics;
+
+    private TraversalMethod traversalMethod;
+    private List<String> mustHaveConceptionEntityUIDs;
+    private List<String> notHaveConceptionEntityUIDs;
+    private List<String> endWithConceptionEntityUIDs;
+    private List<String> terminateAtConceptionEntityUIDs;
 }
