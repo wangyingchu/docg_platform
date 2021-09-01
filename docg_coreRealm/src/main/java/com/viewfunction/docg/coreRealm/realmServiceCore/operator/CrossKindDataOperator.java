5774b8480ef1206f0d782ca9be1ef4e8495a4a13
wangyingchu <yingchuwang@gmail.com>
2021-09-01 13:56:01 +0800

    update for StatisticalAndEvaluable features enhancement

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/operator/CrossKindDataOperator.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/operator/CrossKindDataOperator.java
@@ -7,10 +7,30 @@ import com.viewfunction.docg.coreRealm.realmServiceCore.term.RelationEntity;
 import java.util.List;
 
 public interface CrossKindDataOperator {
-
+    /**
+     * 输入一组概念实体的唯一值ID，查询并返回所有包含其中的概念实体两两间的关系实体数据
+     *
+     * @param conceptionEntityPairUIDs List<String> 概念实体唯一值ID列表
+     *
+     * @return 查询匹配的关系实体对象列表
+     */
     public List<RelationEntity> getRelationsOfConceptionEntityPair(List<String> conceptionEntityPairUIDs) throws CoreRealmServiceEntityExploreException;
 
+    /**
+     * 输入一组关系实体的唯一值ID，返回所有匹配的关系实体数据
+     *
+     * @param relationEntityUIDs List<String> 关系实体唯一值ID列表
+     *
+     * @return 匹配的关系实体对象列表
+     */
     public List<RelationEntity> getRelationEntitiesByUIDs(List<String> relationEntityUIDs) throws CoreRealmServiceEntityExploreException;
 
+    /**
+     * 输入一组概念实体的唯一值ID，返回所有匹配的概念实体数据
+     *
+     * @param conceptionEntityUIDs List<String> 概念实体唯一值ID列表
+     *
+     * @return 匹配的概念实体对象列表
+     */
     public List<ConceptionEntity> getConceptionEntitiesByUIDs(List<String> conceptionEntityUIDs) throws CoreRealmServiceEntityExploreException;
 }
