ca0507567dc80c1d453f2f6d2474d0fc80a521fd
wangyingchu <yingchuwang@gmail.com>
2021-09-10 18:15:29 +0800

    update for SystemMaintenanceOperator features enhancement

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/internal/neo4j/dataTransformer/GetListAttributeSystemInfoTransformer.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/internal/neo4j/dataTransformer/GetListAttributeSystemInfoTransformer.java
@@ -1,12 +1,14 @@
 package com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.dataTransformer;
 
 import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.GraphOperationExecutor;
-import com.viewfunction.docg.coreRealm.realmServiceCore.term.AttributeKind;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.AttributeSystemInfo;
+import org.neo4j.driver.Record;
 import org.neo4j.driver.Result;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class GetListAttributeSystemInfoTransformer implements DataTransformer<List<AttributeKind>>{
+public class GetListAttributeSystemInfoTransformer implements DataTransformer<List<AttributeSystemInfo>>{
 
     private GraphOperationExecutor workingGraphOperationExecutor;
 
@@ -15,7 +17,32 @@ public class GetListAttributeSystemInfoTransformer implements DataTransformer<Li
     }
 
     @Override
-    public List<AttributeKind> transformResult(Result result) {
-        return null;
+    public List<AttributeSystemInfo> transformResult(Result result) {
+        List<AttributeSystemInfo> attributeSystemInfoList = new ArrayList<>();
+        if(result.hasNext()){
+            while(result.hasNext()){
+                Record nodeRecord = result.next();
+                if(nodeRecord != null){
+                    String attributeName = nodeRecord.get("property").asString();
+                    String dataType = nodeRecord.get("type").asString();
+                    boolean usedInIndex = false;
+                    boolean uniqueAttribute = false;
+                    boolean constraintAttribute = false;
+                    if(!nodeRecord.get("isIndexed").isNull()){
+                        usedInIndex = nodeRecord.get("isIndexed").asBoolean();
+                    }
+                    if(!nodeRecord.get("uniqueConstraint").isNull()){
+                        uniqueAttribute = nodeRecord.get("uniqueConstraint").asBoolean();
+                    }
+                    if(!nodeRecord.get("existenceConstraint").isNull()){
+                        constraintAttribute = nodeRecord.get("existenceConstraint").asBoolean();
+                    }
+                    AttributeSystemInfo attributeSystemInfo = new AttributeSystemInfo(attributeName,dataType,usedInIndex,
+                            uniqueAttribute,constraintAttribute);
+                    attributeSystemInfoList.add(attributeSystemInfo);
+                }
+            }
+        }
+        return attributeSystemInfoList;
     }
 }
