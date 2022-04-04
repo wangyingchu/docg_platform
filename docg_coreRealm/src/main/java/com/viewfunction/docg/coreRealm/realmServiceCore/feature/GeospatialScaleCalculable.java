2ce006a7718887ffc87a49b9cd0ebf4f26fbac98
wangyingchu <yingchuwang@gmail.com>
2022-04-04 23:11:02 +0800

    update for GeospatialScaleCalculable feature

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/GeospatialScaleCalculable.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/GeospatialScaleCalculable.java
@@ -1,4 +1,24 @@
 package com.viewfunction.docg.coreRealm.realmServiceCore.feature;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionEntity;
+
+import java.util.List;
+
 public interface GeospatialScaleCalculable {
+
+    public enum SpatialPredicateType {Contains,Intersects,Within,Equals,Crosses,Touches,Overlaps}
+
+    public enum SpatialScaleLevel {Global,Country,Local}
+
+    public List<ConceptionEntity> getSpatialPredicateMatchedConceptionEntities(List<String> targetConceptionKinds,
+                                                                               SpatialPredicateType spatialPredicateType,
+                                                                               SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
+
+    public List<ConceptionEntity> getSpatialBufferMatchedConceptionEntities(List<String> targetConceptionKinds,
+                                                                            double bufferDistanceValue,
+                                                                            SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
+
+    public boolean isSpatialPredicateMatchedWith(String targetConceptionEntityUID,SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
+
 }
--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/spi/neo4j/featureInf/Neo4JGeospatialScaleCalculable.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/spi/neo4j/featureInf/Neo4JGeospatialScaleCalculable.java
@@ -1,6 +1,27 @@
 package com.viewfunction.docg.coreRealm.realmServiceCore.feature.spi.neo4j.featureInf;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
 import com.viewfunction.docg.coreRealm.realmServiceCore.feature.GeospatialScaleCalculable;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionEntity;
+
+import java.util.List;
 
 public interface Neo4JGeospatialScaleCalculable extends GeospatialScaleCalculable {
+
+    default public List<ConceptionEntity> getSpatialPredicateMatchedConceptionEntities(
+            List<String> targetConceptionKinds, SpatialPredicateType spatialPredicateType,
+            SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException{
+        return null;
+    }
+
+    default public List<ConceptionEntity> getSpatialBufferMatchedConceptionEntities(
+            List<String> targetConceptionKinds, double bufferDistanceValue,
+            SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException{
+        return null;
+    }
+
+    default public boolean isSpatialPredicateMatchedWith(
+            String targetConceptionEntityUID, SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException{
+        return false;
+    }
 }
