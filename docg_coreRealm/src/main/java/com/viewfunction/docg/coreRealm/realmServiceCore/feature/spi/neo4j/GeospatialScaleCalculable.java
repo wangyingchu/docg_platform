c948a9f5dbcef0c827da5f1a5d0df6d1dc2f8d2d
wangyingchu <yingchuwang@gmail.com>
2022-04-04 21:33:42 +0800

    update for GeospatialScaleCalculable feature

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/spi/neo4j/GeospatialScaleCalculable.java
+++
@@ -1,4 +0,0 @@
-package com.viewfunction.docg.coreRealm.realmServiceCore.feature.spi.neo4j;
-
-public interface GeospatialScaleCalculable {
-}
---
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/spi/neo4j/featureInf/Neo4JGeospatialScaleCalculable.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.coreRealm.realmServiceCore.feature.spi.neo4j.featureInf;
+
+import com.viewfunction.docg.coreRealm.realmServiceCore.feature.GeospatialScaleCalculable;
+
+public interface Neo4JGeospatialScaleCalculable extends GeospatialScaleCalculable {
+}
