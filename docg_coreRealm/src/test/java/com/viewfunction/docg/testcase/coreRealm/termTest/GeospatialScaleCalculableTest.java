a3886865172c201009f31194fec2ccd6ebad3dd6
wangyingchu <yingchuwang@gmail.com>
2022-04-05 22:59:04 +0800

    update for GeospatialScaleCalculable feature

--- docg_coreRealm/src/test/java/com/viewfunction/docg/testcase/coreRealm/termTest/GeospatialScaleCalculableTest.java
+++ docg_coreRealm/src/test/java/com/viewfunction/docg/testcase/coreRealm/termTest/GeospatialScaleCalculableTest.java
@@ -1,12 +1,22 @@
 package com.viewfunction.docg.testcase.coreRealm.termTest;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceException;
 import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
+import com.viewfunction.docg.coreRealm.realmServiceCore.feature.GeospatialScaleCalculable;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.ConceptionEntityValue;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionEntity;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionKind;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.CoreRealmStorageImplTech;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GeospatialScaleCalculableTest {
 
@@ -26,6 +36,34 @@ public class GeospatialScaleCalculableTest {
 
         Assert.assertEquals(coreRealm.getStorageImplTech(), CoreRealmStorageImplTech.NEO4J);
 
+        ConceptionKind buildingConceptionKind = coreRealm.getConceptionKind("BuildingForGeospatialScaleCalculable");
+        if(buildingConceptionKind == null){
+            buildingConceptionKind = coreRealm.createConceptionKind("BuildingForGeospatialScaleCalculable","buildingForTest");
+        }
+        buildingConceptionKind.purgeAllEntities();
+
+        Map<String,Object> newEntityValue= new HashMap<>();
+        newEntityValue.put("prop1","prop1");
+        newEntityValue.put("prop2","StringValue");
+        ConceptionEntityValue conceptionEntityValue = new ConceptionEntityValue(newEntityValue);
+        ConceptionEntity _ConceptionEntity = buildingConceptionKind.newEntity(conceptionEntityValue,false);
+
+        boolean exceptionShouldHappen = false;
+        List<String> targetKind = new ArrayList<>();
+        targetKind.add("TargetKind1");
+        try {
+            _ConceptionEntity.getSpatialPredicateMatchedConceptionEntities(targetKind, GeospatialScaleCalculable.SpatialPredicateType.Contains,
+                    GeospatialScaleCalculable.SpatialScaleLevel.Country);
+        }catch (CoreRealmServiceException e){
+            exceptionShouldHappen = true;
+        }
+        Assert.assertTrue(exceptionShouldHappen);
+
+
+        //String shpFileLocation = "/home/wangychu/Desktop/GYD_GYW_GIS_DATA/GYD-GIS_WGS84/gyd-building.shp";
+        //String conceptionKindName = "GYD_Building";
+        //File file = new File(shpFileLocation);
+        //SHP_DataSourceImport.importSHPDataToConceptionKind(conceptionKindName,true,file,null);
 
 
     }
