e4d47dcc26474e5ebf9f06aae2133668251a2149
wangyingchu <yingchuwang@gmail.com>
2022-04-05 21:04:03 +0800

    update for GeospatialScaleCalculable feature

--- docg_coreRealm/src/test/java/com/viewfunction/docg/testcase/coreRealm/termTest/GeospatialScaleCalculableTest.java
+++ docg_coreRealm/src/test/java/com/viewfunction/docg/testcase/coreRealm/termTest/GeospatialScaleCalculableTest.java
@@ -1,6 +1,12 @@
 package com.viewfunction.docg.testcase.coreRealm.termTest;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
+import com.viewfunction.docg.coreRealm.realmServiceCore.util.CoreRealmStorageImplTech;
+import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class GeospatialScaleCalculableTest {
 
@@ -11,5 +17,16 @@ public class GeospatialScaleCalculableTest {
         System.out.println("--------------------------------------------------");
         System.out.println("Init unit test data for GeospatialScaleCalculableTest");
         System.out.println("--------------------------------------------------");
+    }
+
+    @Test
+    public void testGeospatialScaleCalculableFunction() throws CoreRealmServiceRuntimeException{
+        CoreRealm coreRealm = RealmTermFactory.getDefaultCoreRealm();
+        //coreRealm.openGlobalSession();
+
+        Assert.assertEquals(coreRealm.getStorageImplTech(), CoreRealmStorageImplTech.NEO4J);
+
+
+
     }
 }
