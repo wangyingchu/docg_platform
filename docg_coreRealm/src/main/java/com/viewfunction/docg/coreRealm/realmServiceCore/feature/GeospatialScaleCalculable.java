e7b602d4559a262d513511d9549ec5842a50d478
wangyingchu <yingchuwang@gmail.com>
2022-04-16 23:51:04 +0800

    update for GeospatialScaleCalculable feature

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/GeospatialScaleCalculable.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/feature/GeospatialScaleCalculable.java
@@ -4,6 +4,7 @@ import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServi
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionEntity;
 
 import java.util.List;
+import java.util.Set;
 
 public interface GeospatialScaleCalculable {
 
@@ -23,11 +24,16 @@ public interface GeospatialScaleCalculable {
     public boolean isSpatialPredicateMatchedWith(SpatialPredicateType spatialPredicateType,String targetConceptionEntityUID,
                                                  SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
 
+    public boolean isSpatialPredicateMatchedWith(SpatialPredicateType spatialPredicateType, Set<String> targetConceptionEntityUIDsSet,
+                                                 SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
+
     public GeospatialScaleFeatureSupportable.WKTGeometryType getEntityGeometryType(SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
 
     public double getEntitiesSpatialDistance(String targetConceptionEntityUID, SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
 
     public boolean isEntitiesWithinSpatialDistance(String targetConceptionEntityUID, double distanceValue, SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
+
+    public boolean isEntitiesWithinSpatialDistance(Set<String> targetConceptionEntityUIDsSet, double distanceValue, SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
 
     public String getEntitySpatialBufferWKTGeometryContent(double bufferDistanceValue,SpatialScaleLevel spatialScaleLevel) throws CoreRealmServiceRuntimeException;
 }
