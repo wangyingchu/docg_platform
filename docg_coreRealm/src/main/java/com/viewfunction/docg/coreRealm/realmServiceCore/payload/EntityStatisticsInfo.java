a6ff8992c63d8654cbd0f6afac63009aca05350c
wangyingchu <yingchuwang@gmail.com>
2022-05-04 17:28:26 +0800

    enhance ConceptionKind entity statistic function

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/payload/EntityStatisticsInfo.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/payload/EntityStatisticsInfo.java
@@ -1,6 +1,9 @@
 package com.viewfunction.docg.coreRealm.realmServiceCore.payload;
 
+import java.util.Date;
+
 public class EntityStatisticsInfo {
+
 
     public enum kindType {ConceptionKind,RelationKind}
 
@@ -8,12 +11,33 @@ public class EntityStatisticsInfo {
     private long entitiesCount;
     private kindType entityKindType;
     private boolean isSystemKind;
+    private String entityKindDesc;
+    private String entityKindUID;
+    private Date createDateTime;
+    private Date lastModifyDateTime;
+    private String creatorId;
+    private String dataOrigin;
 
     public EntityStatisticsInfo(String entityKindName,kindType entityKindType,boolean isSystemKind,long entitiesCount){
         this.entityKindName = entityKindName;
         this.entitiesCount = entitiesCount;
         this.entityKindType = entityKindType;
         this.isSystemKind = isSystemKind;
+    }
+
+    public EntityStatisticsInfo(String entityKindName,kindType entityKindType,boolean isSystemKind,long entitiesCount,
+                                String entityKindDesc,String entityKindUID,Date createDateTime,Date lastModifyDateTime,
+                                String creatorId,String dataOrigin){
+        this.entityKindName = entityKindName;
+        this.entitiesCount = entitiesCount;
+        this.entityKindType = entityKindType;
+        this.isSystemKind = isSystemKind;
+        this.entityKindDesc = entityKindDesc;
+        this.entityKindUID = entityKindUID;
+        this.createDateTime = createDateTime;
+        this.lastModifyDateTime = lastModifyDateTime;
+        this.creatorId = creatorId;
+        this.dataOrigin = dataOrigin;
     }
 
     public String getEntityKindName() {
@@ -32,4 +56,27 @@ public class EntityStatisticsInfo {
         return isSystemKind;
     }
 
+    public String getEntityKindDesc() {
+        return entityKindDesc;
+    }
+
+    public String getEntityKindUID() {
+        return entityKindUID;
+    }
+
+    public Date getCreateDateTime() {
+        return createDateTime;
+    }
+
+    public Date getLastModifyDateTime() {
+        return lastModifyDateTime;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public String getDataOrigin() {
+        return dataOrigin;
+    }
 }
