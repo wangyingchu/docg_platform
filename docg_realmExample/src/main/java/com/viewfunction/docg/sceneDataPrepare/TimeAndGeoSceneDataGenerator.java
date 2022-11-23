68d6eb714875cde8b063f64583d23960de2dea57
wangyingchu <yingchuwang@gmail.com>
2022-11-23 23:31:05 +0800

    add example data

--- docg_realmExample/src/main/java/com/viewfunction/docg/sceneDataPrepare/TimeAndGeoSceneDataGenerator.java
+++ docg_realmExample/src/main/java/com/viewfunction/docg/sceneDataPrepare/TimeAndGeoSceneDataGenerator.java
@@ -1,8 +1,16 @@
 package com.viewfunction.docg.sceneDataPrepare;
 
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceEntityExploreException;
 import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
+import com.viewfunction.docg.realmExample.generator.RoadWeatherInformationStationsRecords_Realm_Generator;
+import com.viewfunction.docg.realmExample.generator.SeattleRealTimeFire911Calls_Realm_Generator;
 
 public interface TimeAndGeoSceneDataGenerator {
 
-    public static void main(String[] args) throws CoreRealmServiceRuntimeException {}
+    public static void main(String[] args) throws CoreRealmServiceRuntimeException, CoreRealmServiceEntityExploreException {
+        //load data
+        SeattleRealTimeFire911Calls_Realm_Generator.main(null);
+        RoadWeatherInformationStationsRecords_Realm_Generator.main(null);
+
+    }
 }
