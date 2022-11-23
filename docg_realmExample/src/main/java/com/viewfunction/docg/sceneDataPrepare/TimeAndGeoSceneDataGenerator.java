a4308421d7f9537c07218733d9c944823c240ba3
wangyingchu <yingchuwang@gmail.com>
2022-11-23 16:38:29 +0800

    Update for TimeAndGeoSceneDataGenerator

---
+++ docg_realmExample/src/main/java/com/viewfunction/docg/sceneDataPrepare/TimeAndGeoSceneDataGenerator.java
@@ -0,0 +1,8 @@
+package com.viewfunction.docg.sceneDataPrepare;
+
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
+
+public interface TimeAndGeoSceneDataGenerator {
+
+    public static void main(String[] args) throws CoreRealmServiceRuntimeException {}
+}
