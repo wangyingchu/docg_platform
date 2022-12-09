f7c6f6b3a1636082e86b015786d60fd60b4f9fca
wangyingchu <yingchuwang@gmail.com>
2022-12-09 14:23:19 +0800

    update launchpad shell to support ignite running on Java 17

--- docg_dataCompute/runtime/launchpad.sh
+++ docg_dataCompute/runtime/launchpad.sh
@@ -25,5 +25,16 @@ done
 #echo $EXTENDCLASSPATH
 
 #Start main program
-# Must add -Xrs here, otherwise when start this shell from nohup, after telnet window closes the java process will closed
-"java" -XX:+UseG1GC -XX:+DisableExplicitGC -Xmx10G -Xrs -cp $EXTENDCLASSPATH:$LIB "com.viewfunction.docg.dataCompute.consoleApplication.DataComputeApplicationLauncher"
+# Must add -Xrs here, otherwise when start this shell from nohup, after telnet window closes the java process will closed, add --add-opens parts to support Java 17
+"java" -XX:+UseG1GC -XX:+DisableExplicitGC -Xmx10G -Xrs \
+--add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED \
+--add-opens=java.base/jdk.internal.misc=ALL-UNNAMED \
+--add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+--add-opens=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED \
+--add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED \
+--add-opens=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED \
+--add-opens=java.base/java.io=ALL-UNNAMED \
+--add-opens=java.base/java.nio=ALL-UNNAMED \
+--add-opens=java.base/java.util=ALL-UNNAMED \
+--add-opens=java.base/java.lang=ALL-UNNAMED \
+-cp $EXTENDCLASSPATH:$LIB "com.viewfunction.docg.dataCompute.consoleApplication.DataComputeApplicationLauncher"
