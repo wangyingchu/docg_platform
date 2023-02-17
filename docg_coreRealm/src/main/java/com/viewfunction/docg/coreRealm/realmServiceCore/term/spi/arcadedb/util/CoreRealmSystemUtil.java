e528d2f0edb383d5a532612f2cdb0133ae9d6451
wangyingchu <yingchuwang@gmail.com>
2023-02-18 07:46:56 +0800

    update for arcadeDB support

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/spi/arcadedb/util/CoreRealmSystemUtil.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/spi/arcadedb/util/CoreRealmSystemUtil.java
@@ -1,9 +1,17 @@
 package com.viewfunction.docg.coreRealm.realmServiceCore.term.spi.arcadedb.util;
 
+import cn.hutool.http.Header;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
 import com.arcadedb.database.DatabaseFactory;
 import com.arcadedb.remote.RemoteDatabase;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.spi.arcadedb.termImpl.ArcadeDBCoreRealmImpl;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.spi.arcadedb.util.httpResponseVO.ListDatabasesVO;
+
+import java.util.Base64;
 
 public class CoreRealmSystemUtil {
 
@@ -45,4 +53,34 @@ public class CoreRealmSystemUtil {
 
 
     }
+
+
+
+
+
+    public static String httpGet(){
+String val =
+        Base64.getEncoder().encodeToString("root:wyc4docg".getBytes());
+System.out.println(val);
+        //HttpResponse httpResponse = HttpUtil.createGet("http://127.0.1.1:2480/api/v1/databases").header(Header.AUTHORIZATION,"Basic cm9vdDp3eWM0ZG9jZw==").execute();
+        HttpResponse httpResponse = HttpUtil.createGet("http://127.0.1.1:2480/api/v1/databases").header(Header.AUTHORIZATION,"Basic "+val).execute();
+        System.out.println(httpResponse.body());
+
+        JSON responseJson = JSONUtil.parse(httpResponse.body());
+        System.out.println(responseJson.toStringPretty());
+        ListDatabasesVO ListDatabasesVO = responseJson.toBean(ListDatabasesVO.class);
+        System.out.println(ListDatabasesVO.getUser());
+        System.out.println(ListDatabasesVO.getResult().length);
+        return null;
+
+
+
+
+
+    }
+
+
+
+
+
 }
