9597052df490550c7caf758c4f3c4dc83df25287
wangyingchu <yingchuwang@gmail.com>
2023-02-21 14:05:18 +0800

    update for arcadeDB CoreRealm implementation

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/internal/arcadeDB/QueryBuilder.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/internal/arcadeDB/QueryBuilder.java
@@ -31,8 +31,30 @@ public class QueryBuilder {
         return operationSQL;
     }
 
-    public static String createTypeDataWithProperties(String[] typeNames, Map<String, Object> properties){
+    public static String createTypeDataWithProperties(String typeNames, Map<String, Object> properties){
+        /*
+        StringBuffer propertiesNameSb = new StringBuffer();
+        StringBuffer propertiesValuePlaceHolderSb = new StringBuffer();
+        propertiesNameSb.append("(");
+        propertiesValuePlaceHolderSb.append("(");
 
+        for(int i = 0; i< propertiesNameList.size(); i++){
+            String currentDataPropertyName = propertiesNameList.get(i);
+            // get dataType for property value validate
+            //String dataType = slicePropertiesMap.get(currentDataPropertyName.toUpperCase());
+            propertiesNameSb.append(currentDataPropertyName);
+            propertiesValuePlaceHolderSb.append("?");
+
+            if(i < propertiesNameList.size() - 1){
+                propertiesNameSb.append(",");
+                propertiesValuePlaceHolderSb.append(",");
+            }
+        }
+        propertiesNameSb.append(")");
+        propertiesValuePlaceHolderSb.append(")");
+
+        String sqlFieldsQuerySQL = "INSERT INTO "+typeNames+" "+propertiesNameSb.toString() +" VALUES "+propertiesValuePlaceHolderSb.toString();
+        */
         return null;
     }
 
