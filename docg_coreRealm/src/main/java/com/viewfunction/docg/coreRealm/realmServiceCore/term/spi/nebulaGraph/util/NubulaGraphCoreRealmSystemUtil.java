22343187b626886d7d0d67f8efbe2126efbe1cd8
wangyingchu <yingchuwang@gmail.com>
2023-02-23 17:53:10 +0800

    update for NubulaGraph database support

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/spi/nebulaGraph/util/NubulaGraphCoreRealmSystemUtil.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/spi/nebulaGraph/util/NubulaGraphCoreRealmSystemUtil.java
@@ -11,6 +11,7 @@ import com.vesoft.nebula.client.graph.exception.NotValidConnectionException;
 import com.vesoft.nebula.client.graph.net.NebulaPool;
 import com.vesoft.nebula.client.graph.net.Session;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.spi.nebulaGraph.termImpl.NebulaGraphCoreRealmImpl;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.config.PropertiesHandler;
 
 import java.io.UnsupportedEncodingException;
@@ -29,6 +30,35 @@ public class NubulaGraphCoreRealmSystemUtil {
     private static String defaultCoreRealmName = PropertiesHandler.getPropertyValue(PropertiesHandler.DEFAULT_REALM_NAME);
 
     public static CoreRealm getDefaultCoreRealm(){
+        Set<String> coreRealmsSet = new HashSet<>();
+        try {
+            NebulaPoolConfig nebulaPoolConfig = new NebulaPoolConfig();
+            nebulaPoolConfig.setMaxConnSize(10);
+            List<HostAddress> addresses = Arrays.asList(new HostAddress(server,Integer.valueOf(portString)));
+            NebulaPool pool = new NebulaPool();
+            pool.init(addresses, nebulaPoolConfig);
+            Session session = pool.getSession(user, password, false);
+            ResultSet resultSet = session.execute("DESCRIBE SPACE "+defaultCoreRealmName+";");
+
+            if(resultSet.getErrorMessage() != null &&resultSet.getErrorMessage().startsWith("SpaceNotFound")){
+                //default core realm not creat yet
+                session.execute("CREATE SPACE IF NOT EXISTS "+ defaultCoreRealmName +
+                        "(\n" +
+                        "    [partition_num = <partition_number>,]\n" +
+                        "    [replica_factor = <replica_number>,]\n" +
+                        "    vid_type = {FIXED_STRING(<N>) | INT[64]}\n" +
+                        "    )"
+                );
+            }else{
+                return new NebulaGraphCoreRealmImpl(defaultCoreRealmName);
+            }
+            session.release();
+            pool.close();
+        } catch (UnknownHostException | NotValidConnectionException | IOErrorException | AuthFailedException |
+                 ClientServerIncompatibleException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 
