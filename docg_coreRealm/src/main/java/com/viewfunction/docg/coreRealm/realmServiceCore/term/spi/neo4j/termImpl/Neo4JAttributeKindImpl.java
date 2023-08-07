06a4b1502bc917607bdbf5b5a2f9282e46faaf6c
wangyingchu <yingchuwang@gmail.com>
2023-08-07 11:59:33 +0800

    update for AttributeKind feature

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/spi/neo4j/termImpl/Neo4JAttributeKindImpl.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/spi/neo4j/termImpl/Neo4JAttributeKindImpl.java
@@ -2,6 +2,7 @@ package com.viewfunction.docg.coreRealm.realmServiceCore.term.spi.neo4j.termImpl
 
 import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.CypherBuilder;
 import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.GraphOperationExecutor;
+import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.dataTransformer.DataTransformer;
 import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.dataTransformer.GetListAttributesViewKindTransformer;
 import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.dataTransformer.GetSingleAttributeValueTransformer;
 import com.viewfunction.docg.coreRealm.realmServiceCore.internal.neo4j.util.CommonOperationUtil;
@@ -11,11 +12,13 @@ import com.viewfunction.docg.coreRealm.realmServiceCore.term.AttributeDataType;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.AttributesViewKind;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.RelationDirection;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.spi.neo4j.termInf.Neo4JAttributeKind;
-
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.RealmConstant;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -98,13 +101,82 @@ public class Neo4JAttributeKindImpl implements Neo4JAttributeKind {
 
     @Override
     public Map<String, Long> getAttributeInConceptionKindDistributionStatistics() {
-        /*
-        MATCH (n:CPC_Member) WHERE (n.ADMINI_LEVEL) IS NOT NULL
-        RETURN count(n)
-        UNION
-        MATCH (n:Company) WHERE (n.ADMINI_LEVEL) IS NOT NULL
-        RETURN count(n)
-        */
+        GraphOperationExecutor workingGraphOperationExecutor = this.graphOperationExecutorHelper.getWorkingGraphOperationExecutor();
+        try{
+            /*
+            "MATCH (attributeKind:DOCG_AttributeKind)<-[DOCG_ViewContainsAttributeKindIs]- " +
+            "(attributesViewKind:DOCG_AttributesViewKind)<-[DOCG_ConceptionContainsViewKindIs]-(conceptionKind:DOCG_ConceptionKind) " +
+            "WHERE id(attributeKind) = 40710969 RETURN DISTINCT conceptionKind.name as operationResult LIMIT 1000000";
+            */
+            String conceptionKindListQuery = "MATCH (attributeKind:"+RealmConstant.AttributeKindClass+")<-["+RealmConstant.AttributesViewKind_AttributeKindRelationClass+"]- " +
+                    "(attributesViewKind:"+RealmConstant.AttributesViewKindClass+")<-["+RealmConstant.ConceptionKind_AttributesViewKindRelationClass+"]-(conceptionKind:"+RealmConstant.ConceptionKindClass+") " +
+                    "WHERE id(attributeKind) = "+this.attributeKindUID+" RETURN DISTINCT conceptionKind.name as "+CypherBuilder.operationResultName+" LIMIT 1000000";
+            logger.debug("Generated Cypher Statement: {}", conceptionKindListQuery);
+
+            ArrayList<String> fixSequenceConceptionKindNameList = new ArrayList<>();
+            DataTransformer<Object> queryConceptionKindNameDataTransformer = new DataTransformer(){
+                @Override
+                public Object transformResult(Result result) {
+                    while(result.hasNext()){
+                        Record record = result.next();
+                        String resultDataProperty = CypherBuilder.operationResultName;
+                        if(record.containsKey(CypherBuilder.operationResultName)){
+                            System.out.println(record.get(resultDataProperty).asString());
+                            fixSequenceConceptionKindNameList.add(record.get(resultDataProperty).asString());
+                        }
+                    }
+                    return null;
+                }
+            };
+            workingGraphOperationExecutor.executeRead(queryConceptionKindNameDataTransformer,conceptionKindListQuery);
+
+            if(fixSequenceConceptionKindNameList.size() > 0){
+                /*
+                MATCH (n:CPC_Member) WHERE (n.ADMINI_LEVEL) IS NOT NULL
+                RETURN count(n)
+                UNION ALL
+                MATCH (n:Company) WHERE (n.ADMINI_LEVEL) IS NOT NULL
+                RETURN count(n)
+                */
+                String queryAttributesCountQueryCql="";
+                for(int i=0;i<fixSequenceConceptionKindNameList.size();i++){
+                    String currentConceptionKindName = fixSequenceConceptionKindNameList.get(i);
+                    queryAttributesCountQueryCql = queryAttributesCountQueryCql +
+                            "MATCH (n:"+currentConceptionKindName+") WHERE (n."+this.attributeKindName+") IS NOT NULL " + "RETURN count(n) AS "+CypherBuilder.operationResultName+"\n";
+                    if(i != fixSequenceConceptionKindNameList.size()-1){
+                        queryAttributesCountQueryCql = queryAttributesCountQueryCql + " UNION ALL \n";
+                    }
+                }
+                logger.debug("Generated Cypher Statement: {}", queryAttributesCountQueryCql);
+
+                List<Long> fixSequenceAttributeInEntityCountList = new ArrayList<>();
+                DataTransformer<Object> queryConceptionEntityCountDataTransformer = new DataTransformer(){
+                    @Override
+                    public Object transformResult(Result result) {
+                        while(result.hasNext()){
+                            Record record = result.next();
+                            String resultDataProperty = CypherBuilder.operationResultName;
+                            if(record.containsKey(CypherBuilder.operationResultName)){
+                                fixSequenceAttributeInEntityCountList.add(record.get(resultDataProperty).asLong());
+                            }
+                        }
+                        return null;
+                    }
+                };
+                workingGraphOperationExecutor.executeRead(queryConceptionEntityCountDataTransformer,queryAttributesCountQueryCql);
+
+                Map<String, Long> attributeInConceptionKindDistributionMap = new HashMap<>();
+                for(int i=0;i<fixSequenceConceptionKindNameList.size();i++){
+                    attributeInConceptionKindDistributionMap.put(
+                            fixSequenceConceptionKindNameList.get(i),
+                            fixSequenceAttributeInEntityCountList.get(i)
+                    );
+                }
+                return attributeInConceptionKindDistributionMap;
+            }
+        } finally {
+            this.graphOperationExecutorHelper.closeWorkingGraphOperationExecutor();
+        }
         return null;
     }
 
