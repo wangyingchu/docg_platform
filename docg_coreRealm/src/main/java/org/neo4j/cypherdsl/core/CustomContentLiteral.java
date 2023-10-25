0beeb92fb9354ba6f78ec10424c0bc6aac6b45f1
wangyingchu <yingchuwang@gmail.com>
2023-10-25 17:11:45 +0800

    update neo4j-cypher-dsl.version to version 2023.7.1

--- docg_coreRealm/src/main/java/org/neo4j/cypherdsl/core/CustomContentLiteral.java
+++ docg_coreRealm/src/main/java/org/neo4j/cypherdsl/core/CustomContentLiteral.java
@@ -2,12 +2,16 @@ package org.neo4j.cypherdsl.core;
 
 public class CustomContentLiteral extends LiteralBase<String> {
 
+    private String content;
+
     public CustomContentLiteral(String content) {
         super(content);
+        this.content = content;
     }
 
     @Override
     public String asString() {
-        return String.valueOf(getContent());
+        //return String.valueOf(getContent());
+        return this.content;
     }
 }
