2892ef37341c05b1ecb1ab6d2b5487354e712e60
wangyingchu <yingchuwang@gmail.com>
2023-12-09 21:56:01 +0800

    Data Compute model code structure refactor

---
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/computeServiceCore/term/spi/ignite/termInf/IgniteDataSlice.java
@@ -0,0 +1,6 @@
+package com.viewfunction.docg.dataCompute.computeServiceCore.term.spi.ignite.termInf;
+
+import com.viewfunction.docg.dataCompute.computeServiceCore.term.DataSlice;
+
+public interface IgniteDataSlice extends DataSlice {
+}
