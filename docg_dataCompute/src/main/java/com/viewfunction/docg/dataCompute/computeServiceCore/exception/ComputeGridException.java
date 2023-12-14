088c29065e84610b4ff09baf77f9ac555ac4117a
wangyingchu <yingchuwang@gmail.com>
2023-12-14 16:19:47 +0800

    update for fixing project build error

--- docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/computeServiceCore/exception/ComputeGridException.java
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/computeServiceCore/exception/ComputeGridException.java
@@ -1,4 +1,12 @@
 package com.viewfunction.docg.dataCompute.computeServiceCore.exception;
 
 public class ComputeGridException extends Exception{
+
+    public ComputeGridException(){
+        super();
+    }
+
+    public ComputeGridException(Throwable cause){
+        super(cause);
+    }
 }
