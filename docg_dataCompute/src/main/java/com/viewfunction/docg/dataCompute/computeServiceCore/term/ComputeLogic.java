8055fb8ea3ae981346b6d12d55564d25e9505db9
wangyingchu <yingchuwang@gmail.com>
2024-01-03 17:25:48 +0800

    update for data compute model version change

---
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/computeServiceCore/term/ComputeLogic.java
@@ -0,0 +1,10 @@
+package com.viewfunction.docg.dataCompute.computeServiceCore.term;
+
+public interface ComputeLogic {
+
+    public void init();
+
+    public void stop();
+
+    public void execute();
+}
