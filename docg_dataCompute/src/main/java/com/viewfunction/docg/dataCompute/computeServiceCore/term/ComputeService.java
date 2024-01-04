1cb3073cc113679002340326bc3e76e90dfc34c9
wangyingchu <yingchuwang@gmail.com>
2024-01-04 17:47:27 +0800

    update for data compute model version change

--- docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/computeServiceCore/term/ComputeService.java
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/computeServiceCore/term/ComputeService.java
@@ -1,4 +1,11 @@
 package com.viewfunction.docg.dataCompute.computeServiceCore.term;
 
 public interface ComputeService extends AutoCloseable{
+
+    public void deployGridComputeFunction(String functionName,ComputeFunction computeFunction);
+
+    public void deployPerUnitComputeFunction(String functionName,ComputeFunction computeFunction);
+
+    public void deployMultipleUnitComputeFunction(String functionName,ComputeFunction computeFunction);
+
 }
