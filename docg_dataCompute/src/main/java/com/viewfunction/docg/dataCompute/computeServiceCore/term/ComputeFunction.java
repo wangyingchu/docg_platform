dc973dc386ded6ed621e7848a489bbcd2b0ec0c6
wangyingchu <yingchuwang@gmail.com>
2024-01-04 17:46:58 +0800

    update for data compute model version change

--- docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/computeServiceCore/term/ComputeFunction.java
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/computeServiceCore/term/ComputeFunction.java
@@ -2,5 +2,4 @@ package com.viewfunction.docg.dataCompute.computeServiceCore.term;
 
 public interface ComputeFunction {
 
-    public void setComputeFunctionCallback(ComputeFunctionCallback computeFunctionCallback);
 }
