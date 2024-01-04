93490158a725813c6a6928560bd47f5088a911c9
wangyingchu <yingchuwang@gmail.com>
2024-01-04 15:36:41 +0800

    update for data compute model version change

---
+++ docg_dataCompute/src/main/java/com/viewfunction/docg/dataCompute/computeServiceCore/term/spi/ignite/termImpl/IgniteComputeServiceImpl.java
@@ -0,0 +1,44 @@
+package com.viewfunction.docg.dataCompute.computeServiceCore.term.spi.ignite.termImpl;
+
+import com.viewfunction.docg.dataCompute.computeServiceCore.internal.ignite.exception.ComputeGridNotActiveException;
+import com.viewfunction.docg.dataCompute.computeServiceCore.internal.ignite.util.UnitIgniteOperationUtil;
+import com.viewfunction.docg.dataCompute.computeServiceCore.term.spi.ignite.termInf.IgniteComputeService;
+import com.viewfunction.docg.dataCompute.computeServiceCore.util.config.DataComputeConfigurationHandler;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+
+public class IgniteComputeServiceImpl implements IgniteComputeService {
+
+    private Ignite invokerIgnite;
+
+    private IgniteComputeServiceImpl(){}
+
+    public void openServiceSession() throws ComputeGridNotActiveException {
+        Ignition.setClientMode(true);
+        this.invokerIgnite = Ignition.start(DataComputeConfigurationHandler.getDataComputeIgniteConfigurationFilePath());
+        UnitIgniteOperationUtil.checkGridActiveStatus(this.invokerIgnite);
+    }
+
+    public void closeServiceSession(){
+        if(this.invokerIgnite !=null){
+            this.invokerIgnite.close();
+        }
+    }
+
+    public static IgniteComputeServiceImpl getServiceInstance() throws ComputeGridNotActiveException {
+        IgniteComputeServiceImpl igniteComputeServiceImpl= new IgniteComputeServiceImpl();
+        try{
+            igniteComputeServiceImpl.openServiceSession();
+        }catch (ComputeGridNotActiveException e){
+            igniteComputeServiceImpl.closeServiceSession();
+            throw e;
+        }
+        return igniteComputeServiceImpl;
+    }
+
+    @Override
+    public void close() throws Exception {
+        closeServiceSession();
+    }
+}
