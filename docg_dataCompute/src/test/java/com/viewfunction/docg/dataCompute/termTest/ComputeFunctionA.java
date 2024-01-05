1b9b238434b1ac310db2bc832578b9f7bfdc2cab
wangyingchu <yingchuwang@gmail.com>
2024-01-05 16:34:58 +0800

    update for data compute model version change

--- docg_dataCompute/src/test/java/com/viewfunction/docg/dataCompute/termTest/ComputeFunctionA.java
+++ docg_dataCompute/src/test/java/com/viewfunction/docg/dataCompute/termTest/ComputeFunctionA.java
@@ -2,7 +2,7 @@ package com.viewfunction.docg.dataCompute.termTest;
 
 import com.viewfunction.docg.dataCompute.computeServiceCore.term.ComputeFunction;
 
-public interface ComputeFunctionA extends ComputeFunction {
+public interface ComputeFunctionA {
 
     public String doSomeThing(String thing);
 }
