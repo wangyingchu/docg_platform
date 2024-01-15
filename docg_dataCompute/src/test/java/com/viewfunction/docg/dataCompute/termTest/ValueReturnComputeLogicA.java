1dedd2b8e95e05a83cdf8cb5bc7af86a6fd053ed
wangyingchu <yingchuwang@gmail.com>
2024-01-15 17:36:53 +0800

    update for data compute model version change

---
+++ docg_dataCompute/src/test/java/com/viewfunction/docg/dataCompute/termTest/ValueReturnComputeLogicA.java
@@ -0,0 +1,10 @@
+package com.viewfunction.docg.dataCompute.termTest;
+
+import com.viewfunction.docg.dataCompute.computeServiceCore.term.spi.ignite.termImpl.BaseIgniteValueReturnComputeLogic;
+
+public class ValueReturnComputeLogicA extends BaseIgniteValueReturnComputeLogic<Integer> {
+    @Override
+    public Integer execute() {
+        return null;
+    }
+}
