5d5c539be5b265f5e6f151a4c7d098462d3a4b0b
wangyingchu <yingchuwang@gmail.com>
2024-03-24 23:42:31 +0800

    update for example data building

---
+++ docg_coreRealm/src/test/java/specialPurposeTestCase/BeijingDT.java
@@ -0,0 +1,71 @@
+package specialPurposeTestCase;
+
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
+
+import java.io.IOException;
+
+public class BeijingDT {
+
+    public static void main(String[] args) throws CoreRealmServiceRuntimeException, IOException {
+        /*
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("TransportationFacility",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_交通设施服务_20220601_213334.shp"),"UTF-8");
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("AccommodationService",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_住宿服务_20220601_213211.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("SportsLeisure",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_体育休闲服务_20220601_213145.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("PublicFacility",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_公共设施_20220601_213553.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("Company",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_公司企业_20220601_213418.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("Healthcare",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_医疗保健服务_20220601_213159.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("ResidentialArea",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_商务住宅_20220601_213227.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("AddressAndPlaceName",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_地名地址信息_20220601_213540.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("IndoorFacility",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_室内设施_20220601_213716.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("GovernmentAgencyAndSocialOrganization",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_政府机构及社会团体_20220601_213247.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("AutomotiveService",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_汽车服务_20220601_212801.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("LifeService",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_生活服务_20220601_213129.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("ScienceEducationAndCulture",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_科教文化服务_20220601_213309.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("ShoppingService",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_购物服务_20220601_213032.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("AccessFacility",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_通行设施_20220601_213746.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("RoadAncillaryFacility",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_道路附属设施_20220601_213422.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("FinancialInsurance",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_金融保险服务_20220601_213341.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("ScenicSpot",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_风景名胜_20220601_213216.shp"),"UTF-8");
+
+        GeospatialOperationUtil.importSHPDataDirectlyToConceptionKind("CateringService",false,
+                new File("/media/wangychu/NSStorage1/Dev_Data/GIS_DATA/矢量数据/北京市POI/ShapeFile/北京市_餐饮服务_20220601_212901.shp"),"UTF-8");
+        */
+    }
+
+
+
+}
