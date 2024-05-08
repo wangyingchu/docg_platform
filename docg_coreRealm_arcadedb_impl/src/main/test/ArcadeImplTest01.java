b89c3361bd25fb85b468ba1235f4f21486c10e8e
wangyingchu <yingchuwang@gmail.com>
2024-05-08 22:54:54 +0800

    update for core realm arcadeDB model structure

---
+++ docg_coreRealm_arcadedb_impl/src/main/test/ArcadeImplTest01.java
@@ -0,0 +1,25 @@
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmFunctionNotSupportedException;
+import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionKind;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
+import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
+
+public class ArcadeImplTest01 {
+
+    public static void main(String[] args) throws CoreRealmServiceRuntimeException, CoreRealmFunctionNotSupportedException {
+
+        CoreRealm defaultCoreRealm = RealmTermFactory.getDefaultCoreRealm();
+        //
+
+       // System.out.println(defaultCoreRealm.getCoreRealmName());
+
+        //CoreRealm targetCoreRealm = RealmTermFactory.createCoreRealm("wycTest002");
+        //System.out.println(targetCoreRealm);
+
+       // System.out.println(RealmTermFactory.listCoreRealms());
+
+        ConceptionKind newConceptionKind = defaultCoreRealm.createConceptionKind("ConceptionKind17","ConceptionKind17描述");
+        System.out.println(newConceptionKind.getConceptionKindName());
+        System.out.println(newConceptionKind.getConceptionKindDesc());
+    }
+}
