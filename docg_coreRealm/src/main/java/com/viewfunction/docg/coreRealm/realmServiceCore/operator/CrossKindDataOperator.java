b08b0804dd821bfca3b4a8bced54cf3b50790c1e
wangyingchu <yingchuwang@gmail.com>
2024-06-11 13:00:16 +0800

    update for documentation writing

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/operator/CrossKindDataOperator.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/operator/CrossKindDataOperator.java
@@ -80,7 +80,7 @@ public interface CrossKindDataOperator {
      * @param topologySimilarityComputeDirection TopologySimilarityComputeDirection 计算使用的关系方向
      * @param relationKindForCompute String 计算使用的关系类型，如为空则基于现存的所有关系进行计算
      *
-     * @return double 类型的相似度数值
+     * @return double 概念实体的相似度数值
      */
     public Double computeConceptionEntityPairTopologySimilarity(String conceptionEntityAUID,String conceptionEntityBUID,
                                                                 TopologySimilarityComputeAlgorithm topologySimilarityComputeAlgorithm,
