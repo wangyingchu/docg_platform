c3c1d05ad0773ab1ee2a2f7dccfaa064dbb17740
wangyingchu <yingchuwang@gmail.com>
2024-08-12 16:02:35 +0800

    update for ComputeGrid TestCase

--- docg_dataCompute/src/test/java/com/viewfunction/docg/testcase/dataCompute/termTest/DataSliceTest.java
+++ docg_dataCompute/src/test/java/com/viewfunction/docg/testcase/dataCompute/termTest/DataSliceTest.java
@@ -1,9 +1,12 @@
 package com.viewfunction.docg.testcase.dataCompute.termTest;
 
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.analysis.query.QueryParameters;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.analysis.query.filteringItem.EqualFilteringItem;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.exception.ComputeGridException;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.payload.DataSliceDetailInfo;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.payload.DataSliceMetaInfo;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.payload.DataSliceOperationResult;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.payload.DataSliceQueryResult;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.ComputeGrid;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.DataService;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.DataSlice;
@@ -78,7 +81,8 @@ public class DataSliceTest {
             Assert.assertTrue(dataSliceDetailInfo1.getPrimaryKeyPropertiesNames().contains("property2".toUpperCase()));
 
             Map<String, Object> dataPropertiesValue0 = new HashMap<>();
-            dataPropertiesValue0.put("property1", "DataProperty1Value" + new Date().getTime());
+            String dateTimestampStr1 = ""+new Date().getTime();
+            dataPropertiesValue0.put("property1", "DataProperty1Value" + dateTimestampStr1);
             dataPropertiesValue0.put("property2", 1);
             dataPropertiesValue0.put("property3", 1238.999d);
             dataPropertiesValue0.put("property4", "PROP$VALUE+"+100001);
@@ -86,7 +90,8 @@ public class DataSliceTest {
             Assert.assertTrue(addDataResult);
 
             Map<String, Object> dataPropertiesValue1 = new HashMap<>();
-            dataPropertiesValue1.put("property1", "DataProperty1Value" + new Date().getTime());
+            String dateTimestampStr2 = ""+new Date().getTime();
+            dataPropertiesValue1.put("property1", "DataProperty1Value" + dateTimestampStr2);
             dataPropertiesValue1.put("property2", 2);
             dataPropertiesValue1.put("property3", 1238.999d);
             dataPropertiesValue1.put("property4", "PROP$VALUE+"+100001);
@@ -95,6 +100,32 @@ public class DataSliceTest {
 
             dataSliceDetailInfo1 =targetComputeGrid.getDataSliceDetail(testDataSliceName);
             Assert.assertEquals(dataSliceDetailInfo1.getPrimaryDataCount(),100002);
+
+            QueryParameters queryParameters = new QueryParameters();
+            queryParameters.setDefaultFilteringItem(new EqualFilteringItem("property1", "DataProperty1Value" + dateTimestampStr1));
+            DataSliceQueryResult dataSliceQueryResult = targetDataSlice.queryDataRecords(queryParameters);
+            Assert.assertNotNull(dataSliceQueryResult);
+            Assert.assertEquals(dataSliceQueryResult.getResultRecords().size(),1);
+            Assert.assertNotNull(dataSliceQueryResult.getStartTime());
+            Assert.assertNotNull(dataSliceQueryResult.getFinishTime());
+            Assert.assertNotNull(dataSliceQueryResult.getOperationSummary());
+            Assert.assertNotNull(dataSliceQueryResult.getQueryLogic());
+
+            Map<String,Object> dataPropertiesValue2 = new HashMap<>();
+            dataPropertiesValue2.put("property1", "DataProperty1Value" + dateTimestampStr2);
+            dataPropertiesValue2.put("property2", 2);
+            Map<String,Object> resultData = targetDataSlice.getDataRecordByPrimaryKeys(dataPropertiesValue2);
+            Assert.assertNotNull(resultData);
+            Assert.assertEquals(resultData.size(),4);
+            Assert.assertEquals(resultData.get("property1".toUpperCase()),"DataProperty1Value" + dateTimestampStr2);
+            Assert.assertEquals(resultData.get("property2".toUpperCase()),2);
+            Assert.assertEquals(resultData.get("property3".toUpperCase()),1238.999d);
+            Assert.assertEquals(resultData.get("property4".toUpperCase()),"PROP$VALUE+"+100001);
+
+
+
+
+
 
             dataService.eraseDataSlice(testDataSliceName);
             targetDataSlice = dataService.getDataSlice(testDataSliceName);
