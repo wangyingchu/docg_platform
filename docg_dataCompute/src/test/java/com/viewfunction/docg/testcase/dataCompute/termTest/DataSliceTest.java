083b61560e8ecffa12f325ee7c2993e022eef557
wangyingchu <yingchuwang@gmail.com>
2024-08-12 13:43:26 +0800

    update for ComputeGrid TestCase

---
+++ docg_dataCompute/src/test/java/com/viewfunction/docg/testcase/dataCompute/termTest/DataSliceTest.java
@@ -0,0 +1,94 @@
+package com.viewfunction.docg.testcase.dataCompute.termTest;
+
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.exception.ComputeGridException;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.payload.DataSliceDetailInfo;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.payload.DataSliceMetaInfo;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.payload.DataSliceOperationResult;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.ComputeGrid;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.DataService;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.DataSlice;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.DataSlicePropertyType;
+import com.viewfunction.docg.dataCompute.dataComputeServiceCore.util.factory.ComputeGridTermFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+public class DataSliceTest {
+    @BeforeTest
+    public void initData(){
+        System.out.println("--------------------------------------------------");
+        System.out.println("Init unit test data for DataSliceTest");
+        System.out.println("--------------------------------------------------");
+    }
+
+    @Test
+    public void testDataSliceFunction() throws ComputeGridException {
+        String testDataSliceName = "testDataSliceFunctionSlice";
+        ComputeGrid targetComputeGrid = ComputeGridTermFactory.getComputeGrid();
+        try(DataService dataService = targetComputeGrid.getDataService()){
+            List<String> dataSliceNameList = dataService.listDataSliceNames();
+            if(dataSliceNameList.contains(testDataSliceName)){
+                dataService.eraseDataSlice(testDataSliceName);
+            }
+
+            Map<String, DataSlicePropertyType> dataSlicePropertyMap = new HashMap<>();
+            dataSlicePropertyMap.put("property1",DataSlicePropertyType.STRING);
+            dataSlicePropertyMap.put("property2",DataSlicePropertyType.INT);
+            dataSlicePropertyMap.put("property3",DataSlicePropertyType.DOUBLE);
+            dataSlicePropertyMap.put("property4",DataSlicePropertyType.STRING);
+            List<String> pkList = new ArrayList<>();
+            pkList.add("property1");
+            pkList.add("property2");
+            DataSlice targetDataSlice = dataService.createPerUnitDataSlice(testDataSliceName,testDataSliceName+"_Group",dataSlicePropertyMap,pkList);
+            Assert.assertNotNull(targetDataSlice);
+
+            List<Map<String,Object>> dataList = new ArrayList<>();
+            for(int i=0;i<100000;i++) {
+                Map<String, Object> dataPropertiesValue = new HashMap<>();
+                dataPropertiesValue.put("property1", "DataProperty1Value" + new Date().getTime());
+                dataPropertiesValue.put("property2", 1000+i);
+                dataPropertiesValue.put("property3", 1238.999d);
+                dataPropertiesValue.put("property4", "PROP$VALUE+"+i);
+                dataList.add(dataPropertiesValue);
+            }
+
+            List<String> propertyList = new ArrayList<>();
+            propertyList.add("property1");
+            propertyList.add("property2");
+            propertyList.add("property3");
+            propertyList.add("property4");
+
+            DataSliceOperationResult addDataResult1 = targetDataSlice.addDataRecords(propertyList,dataList);
+            Assert.assertNotNull(addDataResult1.getOperationSummary());
+            Assert.assertNotNull(addDataResult1.getStartTime());
+            Assert.assertNotNull(addDataResult1.getFinishTime());
+            Assert.assertEquals(addDataResult1.getSuccessItemsCount(),100000);
+            Assert.assertEquals(addDataResult1.getFailItemsCount(),0);
+
+            DataSliceMetaInfo dataSliceMetaInfo1 = targetDataSlice.getDataSliceMetaInfo();
+            Assert.assertEquals(dataSliceMetaInfo1.getPrimaryDataCount(),100000);
+            Assert.assertEquals(dataSliceMetaInfo1.getDataSliceName(),testDataSliceName);
+
+            DataSliceDetailInfo dataSliceDetailInfo1 =targetComputeGrid.getDataSliceDetail(testDataSliceName);
+
+            Assert.assertEquals(dataSliceDetailInfo1.getPrimaryDataCount(),100000);
+            Assert.assertEquals(dataSliceDetailInfo1.getPrimaryKeyPropertiesNames().size(),2);
+            Assert.assertTrue(dataSliceDetailInfo1.getPrimaryKeyPropertiesNames().contains("property1".toUpperCase()));
+            Assert.assertTrue(dataSliceDetailInfo1.getPrimaryKeyPropertiesNames().contains("property2".toUpperCase()));
+
+
+
+
+
+            dataService.eraseDataSlice(testDataSliceName);
+            targetDataSlice = dataService.getDataSlice(testDataSliceName);
+            Assert.assertNull(targetDataSlice);
+        }catch (ComputeGridException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
