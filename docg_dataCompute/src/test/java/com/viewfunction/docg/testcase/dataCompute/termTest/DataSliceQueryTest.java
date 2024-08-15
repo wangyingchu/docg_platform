b8f628c63785e0dc6e872d657196a3829c218520
wangyingchu <yingchuwang@gmail.com>
2024-08-15 10:32:59 +0800

    update for ComputeGrid TestCase

--- docg_dataCompute/src/test/java/com/viewfunction/docg/testcase/dataCompute/termTest/DataSliceQueryTest.java
+++ docg_dataCompute/src/test/java/com/viewfunction/docg/testcase/dataCompute/termTest/DataSliceQueryTest.java
@@ -5,11 +5,14 @@ import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.ComputeGrid
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.DataService;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.DataSlice;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.term.DataSlicePropertyType;
-import com.viewfunction.docg.dataCompute.dataComputeServiceCore.util.common.CoreRealmOperationUtil;
 import com.viewfunction.docg.dataCompute.dataComputeServiceCore.util.factory.ComputeGridTermFactory;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,16 +42,59 @@ public class DataSliceQueryTest {
                 Map<String, DataSlicePropertyType> dataSlicePropertyMap = new HashMap<>();
                 dataSlicePropertyMap.put(StationName,DataSlicePropertyType.STRING);
                 dataSlicePropertyMap.put(StationLocation,DataSlicePropertyType.STRING);
-                dataSlicePropertyMap.put(RecordDateTime,DataSlicePropertyType.DATE);
+                //dataSlicePropertyMap.put(RecordDateTime,DataSlicePropertyType.DATE);
                 dataSlicePropertyMap.put(RecordId,DataSlicePropertyType.STRING);
                 dataSlicePropertyMap.put(RoadSurfaceTemperature,DataSlicePropertyType.DOUBLE);
                 dataSlicePropertyMap.put(AirTemperature,DataSlicePropertyType.DOUBLE);
-                dataSlicePropertyMap.put(CoreRealmOperationUtil.RealmGlobalUID,DataSlicePropertyType.STRING);
 
                 List<String> pkList = new ArrayList<>();
-                pkList.add(CoreRealmOperationUtil.RealmGlobalUID);
+                pkList.add(RecordId);
+                targetDataSlice = dataService.createGridDataSlice(RoadWeatherInformationStationsRecordsDataSlice,"defaultSliceGroup",dataSlicePropertyMap,pkList);
 
-                dataService.createGridDataSlice(RoadWeatherInformationStationsRecordsDataSlice,"defaultSliceGroup",dataSlicePropertyMap,pkList);
+                SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
+                File file = new File("src/test/Road_Weather_Information_Stations.csv");
+
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String tempStr;
+                int lineCount = 0;
+
+                while ((tempStr = reader.readLine()) != null) {
+                    if(lineCount != 0){
+                        Map<String,Object> newEntityValueMap = new HashMap<>();
+                        String[] dataItems = tempStr.split(",");
+
+                        String stationName = dataItems[0];
+                        String stationLocation = dataItems[1];
+                        String recordDateTime = dataItems[2];
+                        String recordId = dataItems[3];
+                        String roadSurfaceTemperature = dataItems[4];
+                        String airTemperature = dataItems[5];
+
+                        newEntityValueMap.put(StationName,stationName);
+                        newEntityValueMap.put(StationLocation,stationLocation);
+                        //newEntityValueMap.put(RecordDateTime,format.parse(recordDateTime));
+                        newEntityValueMap.put(RecordId,recordId);
+                        newEntityValueMap.put(RoadSurfaceTemperature,Double.valueOf(roadSurfaceTemperature));
+                        newEntityValueMap.put(AirTemperature,Double.valueOf(airTemperature));
+
+
+                        System.out.println(format.parse(recordDateTime));
+
+
+                        boolean addResul = targetDataSlice.addDataRecord(newEntityValueMap);
+                        System.out.println(addResul);
+                    }
+
+                    lineCount++;
+                }
+                reader.close();
+
+
+
+                System.out.println(targetDataSlice.getDataSliceMetaInfo().getTotalDataCount());
+
+
+
             }
         } catch (ComputeGridException e) {
             e.printStackTrace();
