804a79bd3bdf5578f367fc290f3e317e4595e210
wangyingchu <yingchuwang@gmail.com>
2024-12-04 16:25:37 +0800

    update for SpatialPropertiesStatisticAndAnalysis feature enhancement

--- docg_analysisProvider/src/main/scala/com/viewfunction/docg/analysisProvider/feature/communicationRouter/AnalysisProviderCommunicationMessageHandler.scala
+++ docg_analysisProvider/src/main/scala/com/viewfunction/docg/analysisProvider/feature/communicationRouter/AnalysisProviderCommunicationMessageHandler.scala
@@ -21,23 +21,13 @@ class AnalysisProviderCommunicationMessageHandler(globalDataAccessor :GlobalData
       communicationMessage match {
         case communicationMessage: String =>
           println(s" $communicationMessage")
-        /*
-        case communicationMessage: com.viewfunction.docg.analysisProvider.feature.communication.messagePayload.AnalyzeTreesCrownAreaInSection =>
-          //senderActor.tell("Reply for AnalyzeTreesCrownAreaInSection Executed "+communicationMessage.getRequestUUID , communicationActor)
-          println(communicationMessage.getTreeCrownType+" "+communicationMessage.getRequestUUID+" "+communicationMessage.getRequestDateTime)
-          val result = EcologicalEnvironmentAnalysis.executeSparkTreesCrownAreaCal2(globalDataAccessor,"TreeCanopy","CommunityReportingArea")
-          val analyseResponse = new AnalyseResponse(communicationMessage.getRequestUUID)
-          analyseResponse.generateMetaInfo()
-          analyseResponse.setResponseData(result)
-          analyseResponse.setResponseDataForm(communicationMessage.getResponseDataForm)
-          senderActor.tell(analyseResponse,communicationActor)
-        */
+
         case communicationMessage: SpatialPropertiesAggregateStatisticRequest =>
           SpatialPropertiesStatisticAndAnalysis.executeSpatialPropertiesAggregateStatistic(
             globalDataAccessor,analyseResponse,communicationMessage.asInstanceOf[SpatialPropertiesAggregateStatisticRequest])
         case communicationMessage: AdministrativeDivisionSpatialCalculateRequest =>
           AdministrativeDivisionBasedSpatialAnalysis.doExecuteDataSliceAdministrativeDivisionSpatialCalculation(
-              globalDataAccessor,analyseResponse,communicationMessage.asInstanceOf[AdministrativeDivisionSpatialCalculateRequest])
+            globalDataAccessor,analyseResponse,communicationMessage.asInstanceOf[AdministrativeDivisionSpatialCalculateRequest])
       }
     }
 
