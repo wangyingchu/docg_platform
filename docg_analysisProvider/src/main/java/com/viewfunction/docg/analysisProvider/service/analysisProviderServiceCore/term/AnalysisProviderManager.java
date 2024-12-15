ab44cfacee9609726ee5a6c34a281b6dcc5740dc
wangyingchu <yingchuwang@gmail.com>
2024-12-15 23:35:15 +0800

    update for Analysis Provider model new feature

---
+++ docg_analysisProvider/src/main/java/com/viewfunction/docg/analysisProvider/service/analysisProviderServiceCore/term/AnalysisProviderManager.java
@@ -0,0 +1,12 @@
+package com.viewfunction.docg.analysisProvider.service.analysisProviderServiceCore.term;
+
+import com.viewfunction.docg.analysisProvider.service.analysisProviderServiceCore.payload.FunctionalFeatureMetaInfo;
+
+import java.util.Set;
+
+public class AnalysisProviderManager {
+
+    public Set<FunctionalFeatureMetaInfo> listFunctionalFeatures(){
+        return null;
+    }
+}
