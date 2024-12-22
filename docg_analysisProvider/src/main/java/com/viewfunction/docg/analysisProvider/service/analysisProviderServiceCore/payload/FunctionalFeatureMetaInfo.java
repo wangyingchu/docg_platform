f114706f8c571a245bd5a420b7205bd2b882ba22
wangyingchu <yingchuwang@gmail.com>
2024-12-22 11:09:50 +0800

    update for Analysis Provider model new feature

--- docg_analysisProvider/src/main/java/com/viewfunction/docg/analysisProvider/service/analysisProviderServiceCore/payload/FunctionalFeatureMetaInfo.java
+++ docg_analysisProvider/src/main/java/com/viewfunction/docg/analysisProvider/service/analysisProviderServiceCore/payload/FunctionalFeatureMetaInfo.java
@@ -4,4 +4,25 @@ public class FunctionalFeatureMetaInfo {
 
     private String functionalFeatureName;
     private String functionalFeatureDescription;
+
+    public FunctionalFeatureMetaInfo(String functionalFeatureName, String functionalFeatureDescription) {
+        this.setFunctionalFeatureName(functionalFeatureName);
+        this.setFunctionalFeatureDescription(functionalFeatureDescription);
+    }
+
+    public String getFunctionalFeatureName() {
+        return functionalFeatureName;
+    }
+
+    public void setFunctionalFeatureName(String functionalFeatureName) {
+        this.functionalFeatureName = functionalFeatureName;
+    }
+
+    public String getFunctionalFeatureDescription() {
+        return functionalFeatureDescription;
+    }
+
+    public void setFunctionalFeatureDescription(String functionalFeatureDescription) {
+        this.functionalFeatureDescription = functionalFeatureDescription;
+    }
 }
