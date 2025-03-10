68e1d055750442bd9f20938acb5b05486b116a5d
wangyingchu <yingchuwang@gmail.com>
2025-03-10 23:26:30 +0800

    Add TuGraph support in CoreRealm and RealmTermFactory

--- docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/spi/tugraph/util/TuGraphCoreRealmSystemUtil.java
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/spi/tugraph/util/TuGraphCoreRealmSystemUtil.java
@@ -5,8 +5,10 @@ import com.viewfunction.docg.coreRealm.realmServiceCore.term.spi.tugraph.termImp
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.config.PropertiesHandler;
 
 public class TuGraphCoreRealmSystemUtil {
+
     private static String defaultCoreRealmName = PropertiesHandler.getPropertyValue(PropertiesHandler.DEFAULT_REALM_NAME);
     private static String defaultDatabaseName = PropertiesHandler.getPropertyValue(PropertiesHandler.NEO4J_DEFAULT_DATABASE_NAME);
+
     public static CoreRealm getDefaultCoreRealm(){
         return new TuGraphCoreRealmImpl(defaultDatabaseName);
     }
