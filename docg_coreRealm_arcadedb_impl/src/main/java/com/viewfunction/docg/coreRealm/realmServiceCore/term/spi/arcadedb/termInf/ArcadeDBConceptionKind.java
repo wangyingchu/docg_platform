dbb3a9d71b5f26a63ffa8961593554f738e6e0a4
wangyingchu <yingchuwang@gmail.com>
2025-03-24 12:08:49 +0800

    remove arcadeDB support

--- docg_coreRealm_arcadedb_impl/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/term/spi/arcadedb/termInf/ArcadeDBConceptionKind.java
+++
@@ -1,6 +0,0 @@
-package com.viewfunction.docg.coreRealm.realmServiceCore.term.spi.arcadedb.termInf;
-
-import com.viewfunction.docg.coreRealm.realmServiceCore.term.ConceptionKind;
-
-public interface ArcadeDBConceptionKind extends ConceptionKind {
-}
