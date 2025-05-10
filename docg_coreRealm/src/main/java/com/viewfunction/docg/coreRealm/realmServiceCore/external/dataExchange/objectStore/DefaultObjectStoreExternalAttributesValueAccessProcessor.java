3614186b6f3252655d4b63ced4477d3907a46a20
wangyingchu <yingchuwang@gmail.com>
2025-05-10 17:47:56 +0800

    Add default external attributes value access processors

---
+++ docg_coreRealm/src/main/java/com/viewfunction/docg/coreRealm/realmServiceCore/external/dataExchange/objectStore/DefaultObjectStoreExternalAttributesValueAccessProcessor.java
@@ -0,0 +1,27 @@
+package com.viewfunction.docg.coreRealm.realmServiceCore.external.dataExchange.objectStore;
+
+import com.viewfunction.docg.coreRealm.realmServiceCore.analysis.query.AttributesParameters;
+import com.viewfunction.docg.coreRealm.realmServiceCore.analysis.query.QueryParameters;
+import com.viewfunction.docg.coreRealm.realmServiceCore.external.dataExchange.ExternalAttributesValueAccessProcessor;
+import com.viewfunction.docg.coreRealm.realmServiceCore.payload.AttributeValue;
+import com.viewfunction.docg.coreRealm.realmServiceCore.term.AttributesViewKind;
+
+import java.util.List;
+import java.util.Map;
+
+public class DefaultObjectStoreExternalAttributesValueAccessProcessor implements ExternalAttributesValueAccessProcessor {
+    @Override
+    public List<Map<String, Object>> getEntityExternalAttributesValues(AttributesViewKind attributesViewKind, QueryParameters queryParameters, List<AttributeValue> attributeValueList) {
+        return List.of();
+    }
+
+    @Override
+    public Long countEntityExternalAttributesValues(AttributesViewKind attributesViewKind, AttributesParameters attributesParameters, List<AttributeValue> attributeValueList) {
+        return 0l;
+    }
+
+    @Override
+    public Long deleteEntityExternalAttributesValues(AttributesViewKind attributesViewKind, AttributesParameters attributesParameters, List<AttributeValue> attributeValueList) {
+        return 0l;
+    }
+}
