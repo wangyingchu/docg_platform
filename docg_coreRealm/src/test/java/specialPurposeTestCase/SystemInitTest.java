eef4785dfb72469c14e287fa9663ba57935d362b
wangyingchu <yingchuwang@gmail.com>
2026-05-05 23:57:10 +0800

    update readme document

--- docg_coreRealm/src/test/java/specialPurposeTestCase/SystemInitTest.java
+++ docg_coreRealm/src/test/java/specialPurposeTestCase/SystemInitTest.java
@@ -3,7 +3,6 @@ package specialPurposeTestCase;
 import com.viewfunction.docg.coreRealm.realmServiceCore.exception.CoreRealmServiceRuntimeException;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.CoreRealm;
 import com.viewfunction.docg.coreRealm.realmServiceCore.term.GeospatialRegion;
-import com.viewfunction.docg.coreRealm.realmServiceCore.term.TimeFlow;
 import com.viewfunction.docg.coreRealm.realmServiceCore.util.factory.RealmTermFactory;
 
 public class SystemInitTest {
@@ -12,8 +11,8 @@ public class SystemInitTest {
         CoreRealm coreRealm = RealmTermFactory.getDefaultCoreRealm();
         GeospatialRegion geospatialRegion = coreRealm.getOrCreateGeospatialRegion();
         geospatialRegion.createGeospatialScaleEntities();
-        TimeFlow timeFlow = coreRealm.getOrCreateTimeFlow();
-        timeFlow.createTimeSpanEntities(2021,false);
+        //TimeFlow timeFlow = coreRealm.getOrCreateTimeFlow();
+        //timeFlow.createTimeSpanEntities(2021,false);
     }
 
 }
